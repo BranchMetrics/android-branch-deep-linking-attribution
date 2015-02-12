582070847090f9a228a071138cf71168d7528d55
qgong <qinwei@branchmetrics.io>
2015-02-11 23:11:09 -0800

    Change to google_advertising_id; added to open too

--- Branch-SDK/src/io/branch/referral/BranchRemoteInterface.java
+++ Branch-SDK/src/io/branch/referral/BranchRemoteInterface.java
@@ -82,7 +82,7 @@ public class BranchRemoteInterface extends RemoteInterface {
 				}
 				String advertisingId = sysObserver_.getAdvertisingId();
 				if (advertisingId != null) {
-					installPost.put("advertising_id", sysObserver_.getAdvertisingId());
+					installPost.put("google_advertising_id", sysObserver_.getAdvertisingId());
 				}
 				installPost.put("debug", debug);
 			} catch (JSONException ex) {
@@ -111,6 +111,10 @@ public class BranchRemoteInterface extends RemoteInterface {
 					openPost.put("os", sysObserver_.getOS());
 				if (!prefHelper_.getLinkClickIdentifier().equals(PrefHelper.NO_STRING_VALUE)) {
 					openPost.put("link_identifier", prefHelper_.getLinkClickIdentifier());
+				}
+				String advertisingId = sysObserver_.getAdvertisingId();
+				if (advertisingId != null) {
+					openPost.put("google_advertising_id", sysObserver_.getAdvertisingId());
 				}
 				openPost.put("debug", debug);
 			} catch (JSONException ex) {
