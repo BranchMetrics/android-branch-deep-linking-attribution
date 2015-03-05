a9850bbd3ca3048fcffcb4e980b646dfcefc52cb
qgong <qinwei@branchmetrics.io>
2015-03-05 15:48:43 -0800

    load test

--- Branch-SDK-TestBed/src/io/branch/branchandroiddemo/test/BranchSDKTests.java
+++ Branch-SDK-TestBed/src/io/branch/branchandroiddemo/test/BranchSDKTests.java
@@ -22,11 +22,14 @@ import android.test.InstrumentationTestCase;
 
 public class BranchSDKTests extends InstrumentationTestCase {
 	
-	CountDownLatch signal;
-	Branch branch;
-	String urlFB, urlTT;
-	String referralCode;
-
+	private Branch branch;
+	private PrefHelper prefHelper;
+	private static String urlFB;
+	private static String referralCode;
+	private static int referralCodeAmount;
+	private static int referralCodeCalculationType;
+	private static int referralCodeLocation;
+	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
@@ -39,10 +42,9 @@ public class BranchSDKTests extends InstrumentationTestCase {
 	public void setUp() throws Exception {
 		super.setUp();
 		
-		signal = new CountDownLatch(1);
 		branch = Branch.getInstance(getInstrumentation().getContext());
-		PrefHelper.getInstance(getInstrumentation().getContext()).disableSmartSession();
-		initSession();
+		prefHelper = PrefHelper.getInstance(getInstrumentation().getContext());
+		prefHelper.disableSmartSession();
 	}
 
 	@After
@@ -52,62 +54,120 @@ public class BranchSDKTests extends InstrumentationTestCase {
 	}
 	
 	private void initSession() throws InterruptedException {
+		final CountDownLatch signal = new CountDownLatch(1);
 		branch.initSession(new BranchReferralInitListener() {
 			@Override
 			public void onInitFinished(JSONObject referringParams, BranchError error) {
 				assertNull(error);
+				signal.countDown();
 			}
 		});
+		signal.await(1, TimeUnit.SECONDS);
 	}
 	
-	public void test1GetShortUrlSyncFailure() {
+	public void test00GetShortUrlSyncFailure() {
 		String url = branch.getShortUrlSync();
 		assertNull(url);
 	}
 	
-	public void testGetShortURL() throws InterruptedException, IOException {
+	public void test02GetShortURLAsync() throws InterruptedException, IOException {
+		initSession();
+		
+		final CountDownLatch signal = new CountDownLatch(1);
 		branch.getShortUrl("facebook", null, null, null, new BranchLinkCreateListener() {
 			@Override
 			public void onLinkCreate(String url, BranchError error) {
 				assertNull(error);
+				assertNotNull(url);
 				assertTrue(url.startsWith("https://bnc.lt/l/"));
 				urlFB = url;
 				
-				branch.getShortUrl("facebook", null, null, null, new BranchLinkCreateListener() {
-					@Override
-					public void onLinkCreate(String url, BranchError error) {
-						assertNull(error);
-						assertSame(url, urlFB);
-						
-						branch.getShortUrl("twitter", null, null, null, new BranchLinkCreateListener() {
-							@Override
-							public void onLinkCreate(String url, BranchError error) {
-								assertNull(error);
-								assertFalse(url.equals(urlFB));
-								urlTT = url;
-										
-								signal.countDown();
-							}
-						});
-					}
-				});
+				signal.countDown();
 			}
 		});
 		signal.await(1, TimeUnit.SECONDS);
-		
-		String url = branch.getShortUrlSync("facebook", null, null, null);
-		assertNotNull(url);
-		assertSame(url, urlFB);
-		
-		url = branch.getShortUrlSync("twitter", null, null, null);
-		assertSame(url, urlTT);
 	}
 	
-	public void testGetRewards() throws InterruptedException {
+	public void test04GetShortURLAsync1Cached() throws InterruptedException {
+		initSession();
+		
+		final CountDownLatch signal = new CountDownLatch(1);
+		branch.getShortUrl("facebook", null, null, null, new BranchLinkCreateListener() {
+			@Override
+			public void onLinkCreate(String url, BranchError error) {
+				assertNull(error);
+				assertNotNull(url);
+				assertSame(url, urlFB);
+				
+				signal.countDown();
+			}
+		});
+		signal.await(1, TimeUnit.SECONDS);
+	}
+	
+	public void test04GetShortURLAsync2Uncached() throws InterruptedException {
+		initSession();
+		
+		final CountDownLatch signal = new CountDownLatch(1);
+		branch.getShortUrl("twitter", null, null, null, new BranchLinkCreateListener() {
+			@Override
+			public void onLinkCreate(String url, BranchError error) {
+				assertNull(error);
+				assertNotNull(url);
+				assertFalse(url.equals(urlFB));
+				
+				signal.countDown();
+			}
+		});
+		signal.await(1, TimeUnit.SECONDS);
+	}
+	
+	public void test04GetShortURLSync() throws InterruptedException {
+		initSession();
+		
+		String url = branch.getShortUrlSync("facebook", null, null, null);
+		assertSame(url, urlFB);
+		
+		url = branch.getShortUrlSync("facebook", "feature1", null, null);
+		assertFalse(url.equals(urlFB));
+	}
+	
+	public void test01SetIdentity() throws InterruptedException {
+		initSession();
+		
+		final CountDownLatch signal = new CountDownLatch(1);
+		prefHelper.setIdentity(PrefHelper.NO_STRING_VALUE);
+		branch.setIdentity("my_great_user", new BranchReferralInitListener() {
+			@Override
+			public void onInitFinished(JSONObject referringParams, BranchError error) {
+				assertNull(error);
+				assertNotNull(referringParams);
+		        assertTrue(prefHelper.getIdentityID().equals("10406190693483182"));
+		        
+		        JSONObject installParams = branch.getFirstReferringParams();
+		        try {
+					assertTrue(installParams.getString("name").equals("test name"));
+					assertTrue(installParams.getString("message").equals("hello there with short url"));
+				} catch (JSONException e) {
+				}
+
+		        signal.countDown();
+			}
+		});
+		signal.await(1, TimeUnit.SECONDS);
+	}
+	
+	public void test03GetRewardsChanged() throws InterruptedException {
+		initSession();
+		
+		final CountDownLatch signal = new CountDownLatch(1);
+		prefHelper.setCreditCount("default", 9999999);
+		
 		branch.loadRewards(new BranchReferralStateChangedListener() {
 			@Override
 			public void onStateChanged(boolean changed, BranchError error) {
 				assertNull(error);
+				assertTrue(changed);
 				
 				signal.countDown();
 			}
@@ -115,38 +175,63 @@ public class BranchSDKTests extends InstrumentationTestCase {
 		signal.await(1, TimeUnit.SECONDS);
 	}
 	
-	public void testReferralCode() throws InterruptedException {
-		// Get
-		branch.getReferralCode("test", 7, new BranchReferralInitListener() {
+	public void testGetRewardsUnchanged() throws InterruptedException {
+		initSession();
+		
+		final CountDownLatch signal = new CountDownLatch(1);
+		prefHelper.setCreditCount("default", prefHelper.getCreditCount("default"));
+		
+		branch.loadRewards(new BranchReferralStateChangedListener() {
 			@Override
-			public void onInitFinished(JSONObject referralCodeJson, BranchError error) {
+			public void onStateChanged(boolean changed, BranchError error) {
 				assertNull(error);
-				try {
-					referralCode = referralCodeJson.getString("referral_code");
-					assertTrue(referralCode.startsWith("test"));
-					assertEquals(referralCodeJson.getJSONObject("metadata").getInt("amount"), 7);
-					assertEquals(Integer.parseInt(referralCodeJson.getString("calculation_type")), Branch.REFERRAL_CODE_AWARD_UNLIMITED);
-					assertEquals(Integer.parseInt(referralCodeJson.getString("location")), Branch.REFERRAL_CODE_LOCATION_REFERRING_USER);
-				} catch (JSONException ignore) {
-				}
+				assertFalse(changed);
 				
 				signal.countDown();
-
 			}
 		});
 		signal.await(1, TimeUnit.SECONDS);
+	}
+	
+	public void testReferralCode1Get() throws InterruptedException {
+		initSession();
 		
-		// Validate
+		final CountDownLatch signalGet = new CountDownLatch(1);
+		branch.getReferralCode(new BranchReferralInitListener() {
+			@Override
+			public void onInitFinished(JSONObject referralCodeJson, BranchError error) {
+				assertNull(error);
+				assertNotNull(referralCodeJson);
+				try {
+					referralCode = referralCodeJson.getString("referral_code");
+					assertNotNull(referralCode);
+					referralCodeAmount = referralCodeJson.getJSONObject("metadata").getInt("amount");
+					referralCodeCalculationType = Integer.parseInt(referralCodeJson.getString("calculation_type"));
+					referralCodeLocation = Integer.parseInt(referralCodeJson.getString("location"));
+				} catch (JSONException ignore) {
+				}
+				
+				signalGet.countDown();
+
+			}
+		});
+		signalGet.await(1, TimeUnit.SECONDS);
+	}
+	
+	public void testReferralCode2Validate() throws InterruptedException {
+		initSession();
+		
 		final CountDownLatch signalValidate = new CountDownLatch(1);
 		branch.validateReferralCode(referralCode, new BranchReferralInitListener() {
 			@Override
 			public void onInitFinished(JSONObject referralCodeJson, BranchError error) {
 				assertNull(error);
+				assertNotNull(referralCodeJson);
 				try {
 					assertTrue(referralCodeJson.getString("referral_code").equals(referralCode));
-					assertEquals(referralCodeJson.getJSONObject("metadata").getInt("amount"), 7);
-					assertEquals(Integer.parseInt(referralCodeJson.getString("calculation_type")), Branch.REFERRAL_CODE_AWARD_UNLIMITED);
-					assertEquals(Integer.parseInt(referralCodeJson.getString("location")), Branch.REFERRAL_CODE_LOCATION_REFERRING_USER);
+					assertEquals(referralCodeJson.getJSONObject("metadata").getInt("amount"), referralCodeAmount);
+					assertEquals(Integer.parseInt(referralCodeJson.getString("calculation_type")), referralCodeCalculationType);
+					assertEquals(Integer.parseInt(referralCodeJson.getString("location")), referralCodeLocation);
 				} catch (JSONException ignore) {
 				}
 				
@@ -155,26 +240,60 @@ public class BranchSDKTests extends InstrumentationTestCase {
 			}
 		});
 		signalValidate.await(1, TimeUnit.SECONDS);
+	}
+	
+	public void testReferralCode3Apply() throws InterruptedException {
+		initSession();
 		
-		// Apply
 		final CountDownLatch signalApply = new CountDownLatch(1);
 		branch.applyReferralCode(referralCode, new BranchReferralInitListener() {
 			@Override
 			public void onInitFinished(JSONObject referralCodeJson, BranchError error) {
 				assertNull(error);
+				assertNotNull(referralCodeJson);
 				try {
 					assertTrue(referralCodeJson.getString("referral_code").equals(referralCode));
-					assertEquals(referralCodeJson.getJSONObject("metadata").getInt("amount"), 7);
-					assertEquals(Integer.parseInt(referralCodeJson.getString("calculation_type")), Branch.REFERRAL_CODE_AWARD_UNLIMITED);
-					assertEquals(Integer.parseInt(referralCodeJson.getString("location")), Branch.REFERRAL_CODE_LOCATION_REFERRING_USER);
+					assertEquals(referralCodeJson.getJSONObject("metadata").getInt("amount"), referralCodeAmount);
+					assertEquals(Integer.parseInt(referralCodeJson.getString("calculation_type")), referralCodeCalculationType);
+					assertEquals(Integer.parseInt(referralCodeJson.getString("location")), referralCodeLocation);
 				} catch (JSONException ignore) {
 				}
 				
 				signalApply.countDown();
-
 			}
 		});
 		signalApply.await(1, TimeUnit.SECONDS);
-
+	}
+	
+	public void testZLoad() throws InterruptedException {
+		initSession();
+		
+		final CountDownLatch signalFinal = new CountDownLatch(1);
+		final CountDownLatch signal = new CountDownLatch(1);
+		for (int i = 0; i < 100; i++) {
+			final int idx = i;
+			branch.getShortUrl(i + "", null, null, null, new BranchLinkCreateListener() {
+				@Override
+				public void onLinkCreate(String url, BranchError error) {
+					assertNull(error);
+					assertNotNull(url);
+					if (idx == 99) {
+						signal.countDown();
+					}
+				}
+			});
+			Thread.sleep(100);
+		}
+		signal.await(30, TimeUnit.SECONDS);
+		
+		branch.getShortUrl(null, "loadTest", null, null, new BranchLinkCreateListener() {
+			@Override
+			public void onLinkCreate(String url, BranchError error) {
+				assertNull(error);
+				assertNotNull(url);
+				signalFinal.countDown();
+			}
+		});
+		signalFinal.await(1, TimeUnit.SECONDS);
 	}
 }
