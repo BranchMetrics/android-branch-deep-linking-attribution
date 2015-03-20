eaeb111449ff587ad09620833197e55f2b9ecd6b
Matt Fenlon <nolnefm@gmail.com>
2015-03-20 18:59:42 +0000

    Added JavaDocs.

--- Branch-SDK/src/io/branch/referral/BranchError.java
+++ Branch-SDK/src/io/branch/referral/BranchError.java
@@ -1,10 +1,26 @@
 package io.branch.referral;
 
+/**
+ * Returns a general error if the server back-end is down.
+ * 
+ * @author Alex Austin
+ */
 public class BranchError {
+	
+	/**
+	 * Returns the message explaining the error.
+	 * 
+	 * @return A {@link String} value that can be used in error logging or for dialog display to the user.
+	 */
 	public String getMessage() {
 		return "Trouble communicating with server. Please try again";
 	}
 	
+	/**
+	 * Overridden toString method for this object; returns the error message rather than the object's address.
+	 * 
+	 * @return A {@link String} value representing the object's current state.
+	 */
 	@Override
 	public String toString() {
 		return getMessage();
