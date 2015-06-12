97fa3c91593f4993238e1c1e8434fe352006b21f
Hubert Chen <hubert@branchmetrics.io>
2015-06-12 22:16:54 +0000

    use abi default/x86

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -12,6 +12,6 @@ if [ ! -e $DEPS ]; then
   echo y | android update sdk -u -a -t build-tools-21.1.2 &&
   echo y | android update sdk -u -a -t sys-img-x86-android-18 &&
   echo y | android update sdk -u -a -t addon-google_apis-google-18 &&
-  echo n | android create avd -n testing -f -t android-18 &&
+  echo n | android create avd -n testing -f -t android-18 default/x86 &&
   touch $DEPS
 fi
