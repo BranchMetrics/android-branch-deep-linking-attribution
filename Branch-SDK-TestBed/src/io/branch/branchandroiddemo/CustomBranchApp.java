78c53a02fc83bb6dda51eac083954a512d3e3ee5
Alex Austin <alexjaustin@gmail.com>
2015-07-02 13:59:46 -0700

    fix main repo

--- Branch-SDK-TestBed/src/io/branch/branchandroiddemo/CustomBranchApp.java
+++ Branch-SDK-TestBed/src/io/branch/branchandroiddemo/CustomBranchApp.java
@@ -1,13 +1,10 @@
 package io.branch.branchandroiddemo;
 
-import com.squareup.leakcanary.LeakCanary;
-
 import io.branch.referral.BranchApp;
 
 public final class CustomBranchApp extends BranchApp {
     @Override
     public void onCreate() {
         super.onCreate();
-        LeakCanary.install(this);
     }
 }
