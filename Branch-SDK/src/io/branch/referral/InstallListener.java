3a865ad6b9fdbbba75568eb4d9bb820e0488b45b
sojan <sojan@branch.io>
2015-08-12 10:56:27 -0700

    Adding  key value check
    Adding check for key value really exist

--- Branch-SDK/src/io/branch/referral/InstallListener.java
+++ Branch-SDK/src/io/branch/referral/InstallListener.java
@@ -36,8 +36,10 @@ public class InstallListener extends BroadcastReceiver {
 
                 for (String referrerParam : referralParams) {
                     String[] keyValue  =  referrerParam.split("=");
-                    referrerMap.put(URLDecoder.decode(keyValue[0],"UTF-8"),URLDecoder.decode(keyValue[1],"UTF-8"));
-                    Log.d("installTest", keyValue[0] +" = " + keyValue[1]);
+                    if(keyValue.length > 1) { // To make sure that there is one key value pair in referrer
+                        referrerMap.put(URLDecoder.decode(keyValue[0], "UTF-8"), URLDecoder.decode(keyValue[1], "UTF-8"));
+                        Log.d("installTest", keyValue[0] + " = " + keyValue[1]);
+                    }
                 }
 
                 if(referrerMap.containsKey(Defines.Jsonkey.LinkClickID.getKey())){
