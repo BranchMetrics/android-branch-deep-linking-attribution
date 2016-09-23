271178e8763df1584db3b74528f5ce0b568c36bd
sojan <sojan@branch.io>
2016-09-23 15:08:57 -0700

    Adding x86 image

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -7,10 +7,10 @@ DEPS="$ANDROID_HOME/installed-dependencies"
 
 if [ ! -e $DEPS ]; then
   cp -r /usr/local/android-sdk-linux $ANDROID_HOME &&
-  echo y | android update sdk -u -a -t android-18 &&
+  echo y | android update sdk -u -a -t android-23 &&
   echo y | android update sdk -u -a -t platform-tools &&
   echo y | android update sdk -u -a -t build-tools-21.1.2 &&
-  echo y | android update sdk -u -a -t sys-img-x86-android-18 &&
+  echo y | android update sdk -u -a -t sys-img-x86-android-23 &&
   echo y | android update sdk -u -a -t addon-google_apis-google-18 &&
   echo n | android create avd -n circleci-android23 -f -t android-23 --abi default/x86 &&
   touch $DEPS
