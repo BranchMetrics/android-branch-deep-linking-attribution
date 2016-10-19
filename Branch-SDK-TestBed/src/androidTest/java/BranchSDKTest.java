0f57898d01b8db5ccaf99a33b7cf860170797264
sojan <sojan@branch.io>
2016-10-19 16:20:07 -0700

    More Instrumentation test cases
    1.Test Sync Link creation
    2.Test Async Link creation
    3.Test Link Cache
    4.Test Link creation time

--- Branch-SDK-TestBed/src/androidTest/java/BranchSDKTest.java
+++ Branch-SDK-TestBed/src/androidTest/java/BranchSDKTest.java
@@ -11,10 +11,13 @@ import android.util.Log;
 
 import org.json.JSONObject;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 import io.branch.branchandroiddemo.MainActivity;
 import io.branch.indexing.BranchUniversalObject;
@@ -30,17 +33,18 @@ import io.branch.referral.util.LinkProperties;
  * </p>
  */
 @RunWith(AndroidJUnit4.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @SmallTest
 public class BranchSDKTest extends InstrumentationTestCase {
     Context context_;
     boolean isInitialised_ = false;
     private static final String TAG = "BranchAndroidTestCase";
+    BranchUniversalObject buo_;
+    LinkProperties linkProperties_;
+    private static String shortUrlCreated_;
+    private static String errorMessage_;
+    private static final int LINK_GEN_WAIT_TIME = 1000; // Link should be created in 1/2 second
 
-
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
-    }
 
     @Before
     public void createBranchInstance() {
@@ -55,11 +59,29 @@ public class BranchSDKTest extends InstrumentationTestCase {
 
         Activity currentActivity = mInstrumentation.waitForMonitor(monitor);
         assertNotNull(currentActivity);
+
+        buo_ = new BranchUniversalObject()
+                .setCanonicalIdentifier("item/1000")
+                .setCanonicalUrl("https://branch.io/deepviews")
+                .setTitle("Test_Title")
+                .setContentDescription("Test_Description ")
+                .setContentImageUrl("https://example.com/mycontent-12345.png")
+                .setContentIndexingMode(BranchUniversalObject.CONTENT_INDEX_MODE.PUBLIC)
+                .setContentType("application/vnd.businessobjects")
+                //.setContentExpiration(new Date(1476566432000L)) // set contents expiration time if applicable
+                .setPrice(5.00, CurrencyType.USD)
+                .addKeyWord("Test_Keyword1")
+                .addKeyWord("Test_Keyword2")
+                .addContentMetadata("Test_Metadata_Key1", "Metadata_value1")
+                .addContentMetadata("Test_Metadata_Key2", "Metadata_value2");
+
+        linkProperties_ = new LinkProperties()
+                .setChannel("Test_channel");
     }
 
     @Test
-    public void testInitSession() {
-        Log.d(TAG, "---- @Test::initSession() ----");
+    public void test00InitSession() {
+        Log.d(TAG, "\n---- @Test::initSession() ----");
         final String[] initErrorMsg = {""};
         final CountDownLatch latch = new CountDownLatch(1);
         Branch.getInstance().initSession(new Branch.BranchReferralInitListener() {
@@ -79,37 +101,52 @@ public class BranchSDKTest extends InstrumentationTestCase {
             e.printStackTrace();
             isInitialised_ = false;
         }
-
         assertTrue("Branch is not initialised " + initErrorMsg[0], isInitialised_);
-
     }
 
     @Test
-    public void testShortLinkCreation() {
-        Log.d(TAG, "---- @Test::getShortUrl() ----");
-        BranchUniversalObject buo = new BranchUniversalObject()
-                .setCanonicalIdentifier("item/1000")
-                .setCanonicalUrl("https://branch.io/deepviews")
-                .setTitle("Test_Title")
-                .setContentDescription("Test_Description ")
-                .setContentImageUrl("https://example.com/mycontent-12345.png")
-                .setContentIndexingMode(BranchUniversalObject.CONTENT_INDEX_MODE.PUBLIC)
-                .setContentType("application/vnd.businessobjects")
-                //.setContentExpiration(new Date(1476566432000L)) // set contents expiration time if applicable
-                .setPrice(5.00, CurrencyType.USD)
-                .addKeyWord("Test_Keyword1")
-                .addKeyWord("Test_Keyword2")
-                .addContentMetadata("Test_Metadata_Key1", "Metadata_value1")
-                .addContentMetadata("Test_Metadata_Key2", "Metadata_value2");
+    public void test01ShortLinkCreation() {
+        Log.d(TAG, "\n---- @Test::getShortUrl() ----");
+        shortUrlCreated_ = buo_.getShortUrl(context_, linkProperties_);
+        Log.d(TAG, "Short url created " + shortUrlCreated_);
 
-        LinkProperties linkProperties = new LinkProperties()
-                .setChannel("Test_channel");
+        boolean isShortLinkCreated = (!TextUtils.isEmpty(shortUrlCreated_)) && (!shortUrlCreated_.contains("/a/"));
+        assertTrue("Branch short link creation failed. Url created is " + shortUrlCreated_, isShortLinkCreated);
+    }
 
-        String url = buo.getShortUrl(context_, linkProperties);
-        Log.d(TAG, "Short url created " + url);
+    @Test
+    public void test02LinkCache() {
+        Log.d(TAG, "\n---- @Test::LinkCache() ----");
+        String url = buo_.getShortUrl(context_, linkProperties_);
+        assertEquals("Link is not retrieved from cache ", shortUrlCreated_, url);
+    }
 
-        boolean isShortLinkCreated = (!TextUtils.isEmpty(url)) && (!url.contains("/a/"));
-        assertTrue("Branch short link creation failed. Url created is " + url, isShortLinkCreated);
+    @Test
+    public void test03ShortLinkGeneration() {
+        Log.d(TAG, "\n---- @Test::ShortLinkGeneration() ----");
+        linkProperties_.setFeature("TestAsyncLink creation");
+        boolean timedOut = false;
+        final CountDownLatch latch = new CountDownLatch(1);
+        final long startTime = System.currentTimeMillis();
+        buo_.generateShortUrl(context_, linkProperties_, new Branch.BranchLinkCreateListener() {
+            @Override
+            public void onLinkCreate(String url, BranchError error) {
+                latch.countDown();
+                if (error != null) {
+                    errorMessage_ = error.getMessage();
+                }
+                boolean isShortLinkGenerated = (!TextUtils.isEmpty(url)) && (!url.contains("/a/"));
+                assertTrue("Branch short link generation failed . Url created is " + shortUrlCreated_ + "\n" + errorMessage_, isShortLinkGenerated);
+                Log.d(TAG, "Link creation completed in " + (System.currentTimeMillis() - startTime) + " milli seconds");
+            }
+        });
+        try {
+            latch.await(LINK_GEN_WAIT_TIME, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            timedOut = true;
+        }
+        assertFalse("Async link creation timed out. Waited for " + LINK_GEN_WAIT_TIME + "milli seconds", timedOut);
     }
 
 
