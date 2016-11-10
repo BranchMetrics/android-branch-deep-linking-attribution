19937ec7eb6874ba9e488ad6e0fcc4ff359550f0
Evan Groth <egroth@gmail.com>
2016-11-10 11:52:24 -0800

    Update CustomBranchApp.java

--- Branch-SDK-TestBed/src/io/branch/branchandroiddemo/CustomBranchApp.java
+++ Branch-SDK-TestBed/src/io/branch/branchandroiddemo/CustomBranchApp.java
@@ -1,15 +1,14 @@
+
 package io.branch.branchandroiddemo;
 
 //import com.squareup.leakcanary.LeakCanary;
 
-import io.branch.referral.Branch;
 import io.branch.referral.BranchApp;
 
 public final class CustomBranchApp extends BranchApp {
     @Override
     public void onCreate() {
         super.onCreate();
-        Branch.enableSimulateInstalls();
         // Uncomment to test memory leak
         // LeakCanary.install(this);
     }
