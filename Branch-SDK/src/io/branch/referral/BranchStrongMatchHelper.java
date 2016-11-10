dac4d13de6d324079700d5a0af70eebc8497239b
sojan <sojan@branch.io>
2016-11-09 16:13:01 -0800

    Fixing few reflection issues

--- Branch-SDK/src/io/branch/referral/BranchStrongMatchHelper.java
+++ Branch-SDK/src/io/branch/referral/BranchStrongMatchHelper.java
@@ -1,15 +1,18 @@
 package io.branch.referral;
 
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.IBinder;
 import android.text.TextUtils;
 import android.util.Log;
 
-import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
 import java.util.List;
 
 /**
@@ -26,8 +29,27 @@ class BranchStrongMatchHelper {
     Object mClient_ = null;
     private static final int STRONG_MATCH_CHECK_TIME_OUT = 500; // Time to wait for strong match check
     private static final long THIRTY_DAYS_EPOCH_MILLI_SEC = 30 * 24 * 60 * 60 * 1000L;
-
     private final Handler timeOutHandler_;
+
+    private boolean isCustomTabsAvailable_ = true;
+
+    Class<?> CustomTabsClientClass;
+    Class<?> CustomServiceTabConnectionClass;
+    Class<?> CustomTabsCallbackClass;
+    Class<?> CustomTabsSessionClass;
+    Class<?> ICustomTabsServiceClass;
+
+    {
+        try {
+            CustomTabsClientClass = Class.forName("android.support.customtabs.CustomTabsClient");
+            CustomServiceTabConnectionClass = Class.forName("android.support.customtabs.CustomTabsServiceConnection");
+            CustomTabsCallbackClass = Class.forName("android.support.customtabs.CustomTabsCallback");
+            CustomTabsSessionClass = Class.forName("android.support.customtabs.CustomTabsSession");
+            ICustomTabsServiceClass = Class.forName("android.support.customtabs.ICustomTabsService");
+        } catch (Throwable t) {
+            isCustomTabsAvailable_ = false;
+        }
+    }
 
 
     private BranchStrongMatchHelper() {
@@ -46,9 +68,11 @@ class BranchStrongMatchHelper {
         //Check if strong match checked in last 30 days
         if (System.currentTimeMillis() - prefHelper.getLastStrongMatchTime() < THIRTY_DAYS_EPOCH_MILLI_SEC) {
             updateStrongMatchCheckFinished(callback);
+        } else if (!isCustomTabsAvailable_) {
+            updateStrongMatchCheckFinished(callback);
         } else {
             try {
-                if (/*deviceInfo.isHardwareIDReal() &&*/ deviceInfo.getHardwareID() != null) {
+                if (/*deviceInfo.isHardwareIDReal() && */deviceInfo.getHardwareID() != null) {
                     final Uri strongMatchUri = buildStrongMatchUrl(cookieMatchDomain, deviceInfo, prefHelper, systemObserver);
                     if (strongMatchUri != null) {
                         timeOutHandler_.postDelayed(new Runnable() {
@@ -58,41 +82,41 @@ class BranchStrongMatchHelper {
                             }
                         }, STRONG_MATCH_CHECK_TIME_OUT);
 
-
-                        final Class<?> CustomTabsClientClass = Class.forName("android.support.customtabs.CustomTabsClient");
-                        final Class<?> CustomServiceTabConnectionClass = Class.forName("android.support.customtabs.CustomTabsServiceConnection");
-                        final Class<?> CustomTabsCallbackClass = Class.forName("android.support.customtabs.CustomTabsCallback");
-                        final Class<?> CustomTabsSessionClass = Class.forName("android.support.customtabs.CustomTabsSession");
-
                         Method bindCustomTabsServiceMethod = CustomTabsClientClass.getMethod("bindCustomTabsService", Context.class, String.class, CustomServiceTabConnectionClass);
                         final Method warmupMethod = CustomTabsClientClass.getMethod("warmup", long.class);
                         final Method newSessionMethod = CustomTabsClientClass.getMethod("newSession", CustomTabsCallbackClass);
                         final Method mayLaunchUrlMethod = CustomTabsSessionClass.getMethod("mayLaunchUrl", Uri.class, Bundle.class, List.class);
 
-                        InvocationHandler CustomServiceTabConnectionHandler = new InvocationHandler() {
+                        // Try connecting to ChromeCustomTab service
+                        // Note : Refer to android.support.customtabs.CustomTabsClient::bindCustomTabsService
+                        Intent intent = new Intent("android.support.customtabs.action.CustomTabsService");
+                        intent.setPackage("com.android.chrome");
+
+                        context.bindService(intent, new MockCustomTabServiceConnection() {
                             @Override
-                            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                                if (method.getName().equals("onCustomTabsServiceConnected") && args[1] != null) {
-                                    mClient_ = CustomTabsClientClass.cast(args[1]);
-                                    if (mClient_ != null) {
+                            public void onCustomTabsServiceConnected(ComponentName var1, Object clientObj) {
+                                mClient_ = CustomTabsClientClass.cast(clientObj);
+                                if (mClient_ != null) {
+                                    try {
                                         warmupMethod.invoke(mClient_, 0);
-                                        Object customTabsSessionObj = newSessionMethod.invoke(mClient_, null);
+                                        Object customTabsSessionObj = newSessionMethod.invoke(mClient_, new Object[]{null});
                                         if (customTabsSessionObj != null) {
                                             mayLaunchUrlMethod.invoke(customTabsSessionObj, strongMatchUri, null, null);
                                             prefHelper.saveLastStrongMatchTime(System.currentTimeMillis());
                                         }
+                                    } catch (Throwable t) {
+                                        mClient_ = null;
+                                        updateStrongMatchCheckFinished(callback);
                                     }
-                                } else if (method.getName().equals("onServiceDisconnected")) {
-                                    mClient_ = null;
-                                    updateStrongMatchCheckFinished(callback);
                                 }
-                                return null;
                             }
-                        };
-                        Object customServiceTabConnListener = Proxy.newProxyInstance(CustomServiceTabConnectionClass.getClassLoader()
-                                , new Class<?>[]{CustomServiceTabConnectionClass}
-                                , CustomServiceTabConnectionHandler);
-                        bindCustomTabsServiceMethod.invoke(CustomTabsClientClass, context, "com.android.chrome", customServiceTabConnListener);
+
+                            @Override
+                            public void onServiceDisconnected(ComponentName name) {
+                                mClient_ = null;
+                                updateStrongMatchCheckFinished(callback);
+                            }
+                        }, Context.BIND_AUTO_CREATE | Context.BIND_WAIVE_PRIORITY);
 
                     } else {
                         updateStrongMatchCheckFinished(callback);
@@ -147,5 +171,31 @@ class BranchStrongMatchHelper {
 
     interface StrongMatchCheckEvents {
         void onStrongMatchCheckFinished();
+    }
+
+
+    // Note : Since Custom service connection is an abstract class in ChromeCustomTab SDK making it impossible to reflect ,
+    //        creating a custom service connection class for Chrome tab service connection and adding implementation for retrieving the CustomTabClient from Binder
+    private abstract class MockCustomTabServiceConnection implements ServiceConnection {
+        public MockCustomTabServiceConnection() {
+        }
+
+        public final void onServiceConnected(final ComponentName name, IBinder service) {
+            try {
+                // Once the service is connected get the customTabClient from binder
+                Constructor<?> customTabClientConstructor = CustomTabsClientClass.getDeclaredConstructor(ICustomTabsServiceClass, ComponentName.class);
+                customTabClientConstructor.setAccessible(true);
+                Class<?> ICustomTabServiceStubClass = Class.forName("android.support.customtabs.ICustomTabsService$Stub");
+                Method asInterfaceMethod = ICustomTabServiceStubClass.getMethod("asInterface", IBinder.class);
+
+                Object customTabClientObject = customTabClientConstructor.newInstance(asInterfaceMethod.invoke(null, service), name);
+                this.onCustomTabsServiceConnected(name, customTabClientObject);
+            } catch (Throwable t) {
+                this.onCustomTabsServiceConnected(null, null);
+            }
+        }
+
+        // Abstract method to mimic same interface as of ChromeCustomTab SDK
+        public abstract void onCustomTabsServiceConnected(ComponentName var1, Object customTabClientObj);
     }
 }
