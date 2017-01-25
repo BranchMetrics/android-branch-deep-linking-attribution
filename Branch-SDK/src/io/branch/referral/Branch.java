0e11cd43206ebb3086f30eede29ed9d7d3daf127
EvangelosG <egroth@gmail.com>
2017-01-25 10:36:27 -0800

    removing unused import

--- Branch-SDK/src/io/branch/referral/Branch.java
+++ Branch-SDK/src/io/branch/referral/Branch.java
@@ -41,7 +41,6 @@ import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
