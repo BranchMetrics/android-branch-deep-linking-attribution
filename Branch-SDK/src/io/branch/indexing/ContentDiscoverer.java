882c3a67529047c8148adfba90ea84b3db9d5f59
EvangelosG <egroth@gmail.com>
2017-04-24 15:04:36 -0700

    remove logging

--- Branch-SDK/src/io/branch/indexing/ContentDiscoverer.java
+++ Branch-SDK/src/io/branch/indexing/ContentDiscoverer.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.os.Handler;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
