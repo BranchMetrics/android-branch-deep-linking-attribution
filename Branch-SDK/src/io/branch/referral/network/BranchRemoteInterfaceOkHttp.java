0819d076522a4d38de5cb3ddf32ee15f7c13b3b3
EvangelosG <egroth@gmail.com>
2017-06-06 10:18:13 -0700

    more life less imports

--- Branch-SDK/src/io/branch/referral/network/BranchRemoteInterfaceOkHttp.java
+++ Branch-SDK/src/io/branch/referral/network/BranchRemoteInterfaceOkHttp.java
@@ -8,7 +8,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
