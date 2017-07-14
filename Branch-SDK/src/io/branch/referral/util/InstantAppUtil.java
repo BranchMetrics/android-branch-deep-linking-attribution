6c4838082d0abde50eb89bea7ff82f35f0013f17
EvangelosG <egroth@gmail.com>
2017-07-14 14:26:05 -0700

    autoformat code for pretty

--- Branch-SDK/src/io/branch/referral/util/InstantAppUtil.java
+++ Branch-SDK/src/io/branch/referral/util/InstantAppUtil.java
@@ -21,26 +21,27 @@ public class InstantAppUtil {
     private static Boolean isInstantApp = null;
     private static Context lastApplicationContext = null;
     private static PackageManagerWrapper packageManagerWrapper = null;
+
     public static boolean isInstantApp(@NonNull Context context) {
-        if(context == null) {
+        if (context == null) {
             throw new IllegalArgumentException("Context must be non-null");
         } else {
             Context applicationContext = context.getApplicationContext();
-            if(applicationContext == null) {
+            if (applicationContext == null) {
                 throw new IllegalStateException("Application context is null!");
-            } else if(isInstantApp != null && applicationContext.equals(lastApplicationContext)) {
+            } else if (isInstantApp != null && applicationContext.equals(lastApplicationContext)) {
                 return isInstantApp.booleanValue();
             } else {
                 isInstantApp = null;
                 Boolean isInstantAppResult = null;
-                if(isAtLeastO()) {
-                    if(packageManagerWrapper == null || !applicationContext.equals(lastApplicationContext)) {
+                if (isAtLeastO()) {
+                    if (packageManagerWrapper == null || !applicationContext.equals(lastApplicationContext)) {
                         packageManagerWrapper = new PackageManagerWrapper(applicationContext.getPackageManager());
                     }
                     isInstantAppResult = packageManagerWrapper.isInstantApp();
                 }
                 lastApplicationContext = applicationContext;
-                if(isInstantAppResult != null) {
+                if (isInstantAppResult != null) {
                     isInstantApp = isInstantAppResult;
                 } else {
                     try {
@@ -54,21 +55,24 @@ public class InstantAppUtil {
             }
         }
     }
+
     private static boolean isAtLeastO() {
         return Build.VERSION.SDK_INT > 25 || isPreReleaseOBuild();
     }
+
     private static boolean isPreReleaseOBuild() {
         return !"REL".equals(Build.VERSION.CODENAME) && ("O".equals(Build.VERSION.CODENAME) || Build.VERSION.CODENAME.startsWith("OMR"));
     }
+
     public static boolean showInstallPrompt(@NonNull Activity activity, int requestCode, @Nullable String referrer) {
-        if(activity == null) {
+        if (activity == null) {
             throw new IllegalArgumentException("Activity must be non-null");
-        } else if(!isInstantApp(activity)) {
+        } else if (!isInstantApp(activity)) {
             return false;
         } else {
             Intent intent = (new Intent("android.intent.action.VIEW")).setPackage("com.android.vending").addCategory("android.intent.category.DEFAULT").putExtra("callerId", activity.getPackageName()).putExtra("overlay", true);
             Builder uriBuilder = (new Builder()).scheme("market").authority("details").appendQueryParameter("id", activity.getPackageName());
-            if(!TextUtils.isEmpty(referrer)) {
+            if (!TextUtils.isEmpty(referrer)) {
                 uriBuilder.appendQueryParameter("referrer", referrer);
             }
             intent.setData(uriBuilder.build());
@@ -76,17 +80,20 @@ public class InstantAppUtil {
             return true;
         }
     }
+
     static class PackageManagerWrapper {
         private final PackageManager packageManager;
         private static Method isInstantAppMethod;
+
         PackageManagerWrapper(PackageManager packageManager) {
             this.packageManager = packageManager;
         }
+
         Boolean isInstantApp() {
-            if(isAtLeastO()) {
+            if (isAtLeastO()) {
                 return null;
             } else {
-                if(isInstantAppMethod == null) {
+                if (isInstantAppMethod == null) {
                     try {
                         isInstantAppMethod = PackageManager.class.getDeclaredMethod("isInstantApp", new Class[0]);
                     } catch (NoSuchMethodException var3) {
@@ -94,10 +101,10 @@ public class InstantAppUtil {
                     }
                 }
                 try {
-                    return (Boolean)isInstantAppMethod.invoke(this.packageManager, new Object[0]);
+                    return (Boolean) isInstantAppMethod.invoke(this.packageManager, new Object[0]);
                 } catch (IllegalAccessException var2) {
                     return null;
-                } catch ( InvocationTargetException var2) {
+                } catch (InvocationTargetException var2) {
                     return null;
                 }
             }
