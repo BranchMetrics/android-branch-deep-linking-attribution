e4989cd2d1d8d3ff6d2e1a8058fe02bef5c14388
sojan <sojan@branch.io>
2017-12-01 14:13:34 -0800

    Fixing issue : IDL is not triggered with the fix for orientation change handling

--- Branch-SDK/src/io/branch/referral/Branch.java
+++ Branch-SDK/src/io/branch/referral/Branch.java
@@ -1331,7 +1331,7 @@ public class Branch implements BranchViewHandler.IBranchViewEvents, SystemObserv
     private boolean readAndStripParam(Uri data, Activity activity) {
         if (intentState_ == INTENT_STATE.READY) {
             // Check for instant deep linking possibility first
-            if (activity != null && activity.getIntent() != null && initState_ == SESSION_STATE.UNINITIALISED && !checkIntentForSessionRestart(activity.getIntent())) {
+            if (activity != null && activity.getIntent() != null && initState_ != SESSION_STATE.INITIALISED && !checkIntentForSessionRestart(activity.getIntent())) {
                 Intent intent = activity.getIntent();
                 // In case of a cold start by clicking app icon or bringing app to foreground Branch link click is always false.
                 if (intent.getData() == null || isIntentParamsAlreadyConsumed(activity)) {
