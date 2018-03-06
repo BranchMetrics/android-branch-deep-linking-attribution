1b8eaf42616b714da038be1b300ff6c87feb60e6
sojan <sojan@branch.io>
2018-03-06 15:29:38 -0800

    Taking update skip url scheme off the request queue

--- Branch-SDK/src/io/branch/referral/UniversalResourceAnalyser.java
+++ Branch-SDK/src/io/branch/referral/UniversalResourceAnalyser.java
@@ -7,10 +7,15 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import javax.net.ssl.HttpsURLConnection;
 
 /**
  * Created by sojanpr on 2/8/18.
@@ -21,7 +26,7 @@ import java.util.regex.Pattern;
  */
 
 class UniversalResourceAnalyser {
-    private JSONObject skipURLFormats;
+    private static JSONObject skipURLFormats;
     private final ArrayList<String> acceptURLFormats;
     private static final String SKIP_URL_FORMATS_KEY = "skip_url_format_key";
     private static final String VERSION_KEY = "version";
@@ -97,8 +102,9 @@ class UniversalResourceAnalyser {
     }
     
     void checkAndUpdateSkipURLFormats(Context context) {
-        if (Branch.getInstance() != null) {
-            Branch.getInstance().handleNewRequest(new BranchSkipURLUpdateRequest(context, ""));
+        try {
+            new UrlSkipListUpdateTask(context).executeTask();
+        } catch (Throwable ignore) {
         }
     }
     
@@ -139,44 +145,48 @@ class UniversalResourceAnalyser {
         return strippedURL;
     }
     
-    private class BranchSkipURLUpdateRequest extends ServerRequest {
+    private static class UrlSkipListUpdateTask extends BranchAsyncTask<Void, Void, JSONObject> {
+        private final PrefHelper prefHelper;
+        private final int TIME_OUT = 1500;
         
-        BranchSkipURLUpdateRequest(Context context, String requestPath) {
-            super(context, requestPath);
+        private UrlSkipListUpdateTask(Context context) {
+            this.prefHelper = PrefHelper.getInstance(context);
         }
         
         @Override
-        public String getRequestUrl() {
-            return UPDATE_URL_PATH.replace("#", Integer.toString(skipURLFormats.optInt(VERSION_KEY) + 1));
+        protected JSONObject doInBackground(Void... params) {
+            
+            JSONObject respObject = new JSONObject();
+            HttpsURLConnection connection = null;
+            try {
+                URL urlObject = new URL(UPDATE_URL_PATH.replace("#", Integer.toString(skipURLFormats.optInt(VERSION_KEY) + 1)));
+                connection = (HttpsURLConnection) urlObject.openConnection();
+                connection.setConnectTimeout(TIME_OUT);
+                connection.setReadTimeout(TIME_OUT);
+                int responseCode = connection.getResponseCode();
+                if (responseCode == HttpsURLConnection.HTTP_OK) {
+                    if (connection.getInputStream() != null) {
+                        BufferedReader rd = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                        respObject = new JSONObject(rd.readLine());
+                    }
+                }
+            } catch (Throwable ignore) {
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+            }
+            return respObject;
         }
         
         @Override
-        public boolean handleErrors(Context context) {
-            return false;
-        }
-        
-        @Override
-        public void onRequestSucceeded(ServerResponse response, Branch branch) {
-            JSONObject updatedURLFormatsObj = response.getObject();
+        protected void onPostExecute(JSONObject updatedURLFormatsObj) {
+            super.onPostExecute(updatedURLFormatsObj);
             if (updatedURLFormatsObj.optInt(VERSION_KEY) > skipURLFormats.optInt(VERSION_KEY)) {
                 skipURLFormats = updatedURLFormatsObj;
-                prefHelper_.setString(SKIP_URL_FORMATS_KEY, skipURLFormats.toString());
+                prefHelper.setString(SKIP_URL_FORMATS_KEY, skipURLFormats.toString());
             }
         }
-        
-        @Override
-        public void handleFailure(int statusCode, String causeMsg) {
-        
-        }
-        
-        @Override
-        public boolean isGetRequest() {
-            return true;
-        }
-        
-        @Override
-        public void clearCallbacks() {
-        
-        }
     }
+    
 }
