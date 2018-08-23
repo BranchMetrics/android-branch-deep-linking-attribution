8fbb2818f05d8ae601043c955957edc0a85855ed
Aaron Lopez <a.aaron.lopez@gmail.com>
2018-08-23 12:59:03 -0700

    Remove input stream

--- Branch-SDK/src/io/branch/referral/network/BranchRemoteInterfaceUrlConnection.java
+++ Branch-SDK/src/io/branch/referral/network/BranchRemoteInterfaceUrlConnection.java
@@ -153,19 +153,14 @@ public class BranchRemoteInterfaceUrlConnection extends BranchRemoteInterface {
                 InputStream inputStream = null;
                 try {
                     if (responseCode != HttpsURLConnection.HTTP_OK && connection.getErrorStream() != null) {
-                        inputStream = connection.getErrorStream();
+                        return new BranchResponse(getResponseString(connection.getErrorStream()), responseCode);
                     } else {
-                        inputStream = connection.getInputStream();
+                        return new BranchResponse(getResponseString(connection.getInputStream()), responseCode);
                     }
-                    return new BranchResponse(getResponseString(inputStream), responseCode);
                 } catch (FileNotFoundException ex) {
                     // In case of Resource conflict getInputStream will throw FileNotFoundException. Handle it here in order to send the right status code
                     PrefHelper.Debug("BranchSDK", "A resource conflict occurred with this request " + url);
                     return new BranchResponse(null, responseCode);
-                } finally {
-                    if (inputStream != null) {
-                        inputStream.close();
-                    }
                 }
             }
 
