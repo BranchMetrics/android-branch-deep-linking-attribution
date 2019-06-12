e549768f1e1b4dcc6cf96f611c044c037f41d491
Andy Peterson <apeterson@branch.io>
2019-06-12 16:38:08 -0700

    Updated to AndroidX dependencies

--- Branch-SDK/src/io/branch/indexing/BranchUniversalObject.java
+++ Branch-SDK/src/io/branch/indexing/BranchUniversalObject.java
@@ -4,8 +4,8 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Parcel;
 import android.os.Parcelable;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import android.text.TextUtils;
 
 import org.json.JSONArray;
