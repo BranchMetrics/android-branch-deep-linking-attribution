a68c8981c7f45335fd29295672616ec059eff6d5
Ernest Cho <echo@branch.io>
2019-08-02 11:01:30 -0700

    SDK-421 asserts were in the wrong order

--- Branch-SDK/androidTest/io/branch/referral/PrefHelperTest.java
+++ Branch-SDK/androidTest/io/branch/referral/PrefHelperTest.java
@@ -9,17 +9,21 @@ import org.junit.Test;
 
 public class PrefHelperTest extends BranchTest {
 
-    @Test
-    public void testGetAPIBaseUrl() {
+    void assertDefaultURL() {
         Context context = getTestContext();
         PrefHelper helper = PrefHelper.getInstance(context);
         String actual = helper.getAPIBaseUrl();
 
         if (Build.VERSION.SDK_INT >= 20) {
-            Assert.assertEquals(PrefHelper.BRANCH_BASE_URL_V1, actual);
-        } else {
             Assert.assertEquals(PrefHelper.BRANCH_BASE_URL_V2, actual);
+        } else {
+            Assert.assertEquals(PrefHelper.BRANCH_BASE_URL_V1, actual);
         }
+    }
+
+    @Test
+    public void testGetAPIBaseUrl() {
+        assertDefaultURL();
     }
 
     @Test
@@ -35,45 +39,18 @@ public class PrefHelperTest extends BranchTest {
     @Test
     public void testSetAPIUrl_InvalidHttp() {
         PrefHelper.setAPIUrl("http://www.example.com/");
-
-        Context context = getTestContext();
-        PrefHelper helper = PrefHelper.getInstance(context);
-        String actual = helper.getAPIBaseUrl();
-
-        if (Build.VERSION.SDK_INT >= 20) {
-            Assert.assertEquals(PrefHelper.BRANCH_BASE_URL_V1, actual);
-        } else {
-            Assert.assertEquals(PrefHelper.BRANCH_BASE_URL_V2, actual);
-        }
+        assertDefaultURL();
     }
 
     @Test
     public void testSetAPIUrl_InvalidNull() {
         PrefHelper.setAPIUrl(null);
-
-        Context context = getTestContext();
-        PrefHelper helper = PrefHelper.getInstance(context);
-        String actual = helper.getAPIBaseUrl();
-
-        if (Build.VERSION.SDK_INT >= 20) {
-            Assert.assertEquals(PrefHelper.BRANCH_BASE_URL_V1, actual);
-        } else {
-            Assert.assertEquals(PrefHelper.BRANCH_BASE_URL_V2, actual);
-        }
+        assertDefaultURL();
     }
 
     @Test
     public void testSetAPIUrl_InvalidEmpty() {
         PrefHelper.setAPIUrl("");
-
-        Context context = getTestContext();
-        PrefHelper helper = PrefHelper.getInstance(context);
-        String actual = helper.getAPIBaseUrl();
-
-        if (Build.VERSION.SDK_INT >= 20) {
-            Assert.assertEquals(PrefHelper.BRANCH_BASE_URL_V1, actual);
-        } else {
-            Assert.assertEquals(PrefHelper.BRANCH_BASE_URL_V2, actual);
-        }
+        assertDefaultURL();
     }
 }
