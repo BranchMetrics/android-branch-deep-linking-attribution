1b7ba44d08a2461b54e89787fc6a668152c36d4f
bklastaitis-branch <benas.klastaitis@branch.io>
2020-01-22 13:50:44 -0800

    fixed comments

--- Branch-SDK/src/main/java/io/branch/referral/util/BranchCrossPlatformId.java
+++ Branch-SDK/src/main/java/io/branch/referral/util/BranchCrossPlatformId.java
@@ -4,12 +4,7 @@ import io.branch.referral.BranchError;
 
 public class BranchCrossPlatformId {
 
-    // this may look like stupid design but it's an attempt to hotfix a bug,
-    // when we released v4.2.1, we changed the BranchCrossPlatformIdListener signature from
-    // from io.branch.referral.util.BranchCrossPlatformId.BranchCrossPlatformIdListener
-    // to io.branch.referral.ServerRequestGetCPID.BranchCrossPlatformIdListener
-    // and plugins that dynamically pick up the latest SDK version started breaking, so now we have
-    // duplicated BranchCrossPlatformIdListener classes
+    // BranchCrossPlatformIdListener is duplicated for backwards compatibility (fixes SDK-786)
 
     public interface BranchCrossPlatformIdListener {
         void onDataFetched(BranchCPID branchCPID, BranchError error);
--- Branch-SDK/src/main/java/io/branch/referral/util/BranchLastAttributedTouchData.java
+++ Branch-SDK/src/main/java/io/branch/referral/util/BranchLastAttributedTouchData.java
@@ -6,12 +6,7 @@ import io.branch.referral.BranchError;
 
 public class BranchLastAttributedTouchData {
 
-    // this may look like stupid design but it's an attempt to hotfix a bug,
-    // when we released v4.2.1, we changed the BranchLastAttributedTouchDataListener signature from
-    // from io.branch.referral.util.BranchCrossPlatformId.BranchLastAttributedTouchDataListener
-    // to io.branch.referral.ServerRequestGetLATD.BranchLastAttributedTouchDataListener
-    // and plugins that dynamically pick up the latest SDK version started breaking, so now we have
-    // duplicated BranchLastAttributedTouchDataListener classes
+    // BranchLastAttributedTouchDataListener is duplicated for backwards compatibility (fixes SDK-786)
 
     public interface BranchLastAttributedTouchDataListener {
         void onDataFetched(JSONObject jsonObject, BranchError error);
