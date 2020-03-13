12158e1b3960a19b4d915268434adf6d503ff3fb
Jimmy Dee <jimmy.dee@branch.io>
2020-03-13 15:27:25 -0700

    Added a Settings button at the bottom of the main activity to start the Settings activity.

--- Branch-SDK-TestBed/src/main/java/io/branch/branchandroiddemo/SettingsActivity.java
+++ Branch-SDK-TestBed/src/main/java/io/branch/branchandroiddemo/SettingsActivity.java
@@ -1,23 +1,25 @@
 package io.branch.branchandroiddemo;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Switch;
 
 import io.branch.referral.Branch;
-import io.branch.referral.Defines;
 import io.branch.referral.PrefHelper;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        setupDisableAdNetworkCalloutsSwitch();
+     }
+
+    void setupDisableAdNetworkCalloutsSwitch() {
         final Switch disableAdNetworkCalloutsSwitch = findViewById(R.id.disable_ad_network_callouts);
 
         /*
@@ -35,5 +37,6 @@ public class SettingsActivity extends AppCompatActivity {
                 Branch.getInstance().disableAdNetworkCallouts(disableAdNetworkCalloutsSwitch.isChecked());
             }
         });
+
     }
 }
