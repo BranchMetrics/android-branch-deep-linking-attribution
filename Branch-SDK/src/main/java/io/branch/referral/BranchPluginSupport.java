291f40c9ba63474afe6f3c2d218d095c4b4de44d
Nipun <nipun.singh@branch.io>
2022-01-27 17:50:41 -0800

    Created tests and updated deviceDescription
    Added unit tests for BranchPluginSupport and changed deviceDescription() to return a Map instead of a JSONObject.

--- Branch-SDK/src/main/java/io/branch/referral/BranchPluginSupport.java
+++ Branch-SDK/src/main/java/io/branch/referral/BranchPluginSupport.java
@@ -1,17 +1,11 @@
 package io.branch.referral;
 
-import android.app.UiModeManager;
 import android.content.Context;
-import android.content.res.Configuration;
-import android.os.Build;
 import android.text.TextUtils;
 import android.util.DisplayMetrics;
 
-import io.branch.referral.Defines.ModuleNameKeys;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import static android.content.Context.UI_MODE_SERVICE;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BranchPluginSupport {
     private final SystemObserver systemObserver_;
@@ -33,57 +27,54 @@ public class BranchPluginSupport {
         systemObserver_ = new BranchPluginSupport.SystemObserverInstance();
     }
 
-    public JSONObject deviceDescription() {
-        JSONObject deviceDataObj = new JSONObject();
+    public Map<String, Object> deviceDescription() {
+        Map<String, Object> deviceDataObj = new HashMap<String, Object>();
 
-        try {
-            String osName = SystemObserver.getOS(context_);
-            if (!isNullOrEmptyOrBlank(osName)) {
-                deviceDataObj.put(Defines.Jsonkey.OS.getKey(), osName);
-            }
-            deviceDataObj.put(Defines.Jsonkey.OSVersionAndroid.getKey(), SystemObserver.getOSVersion());
+        String osName = SystemObserver.getOS(context_);
+        if (!isNullOrEmptyOrBlank(osName)) {
+            deviceDataObj.put(Defines.Jsonkey.OS.getKey(), osName);
+        }
+        deviceDataObj.put(Defines.Jsonkey.OSVersionAndroid.getKey(), SystemObserver.getOSVersion());
 
-            SystemObserver.UniqueId hardwareID = getHardwareID();
-            if (!isNullOrEmptyOrBlank(hardwareID.getId()) && hardwareID.isReal()) {
-                deviceDataObj.put(Defines.Jsonkey.AndroidID.getKey(), hardwareID.getId());
-            } else {
-                deviceDataObj.put(Defines.Jsonkey.UnidentifiedDevice.getKey(), true);
-            }
+        SystemObserver.UniqueId hardwareID = getHardwareID();
+        if (!isNullOrEmptyOrBlank(hardwareID.getId()) && hardwareID.isReal()) {
+            deviceDataObj.put(Defines.Jsonkey.AndroidID.getKey(), hardwareID.getId());
+        } else {
+            deviceDataObj.put(Defines.Jsonkey.UnidentifiedDevice.getKey(), true);
+        }
 
-            String countryCode = SystemObserver.getISO2CountryCode();
-            if (!TextUtils.isEmpty(countryCode)) {
-                deviceDataObj.put(Defines.Jsonkey.Country.getKey(), countryCode);
-            }
+        String countryCode = SystemObserver.getISO2CountryCode();
+        if (!TextUtils.isEmpty(countryCode)) {
+            deviceDataObj.put(Defines.Jsonkey.Country.getKey(), countryCode);
+        }
 
-            String languageCode = SystemObserver.getISO2LanguageCode();
-            if (!TextUtils.isEmpty(languageCode)) {
-                deviceDataObj.put(Defines.Jsonkey.Language.getKey(), languageCode);
-            }
+        String languageCode = SystemObserver.getISO2LanguageCode();
+        if (!TextUtils.isEmpty(languageCode)) {
+            deviceDataObj.put(Defines.Jsonkey.Language.getKey(), languageCode);
+        }
 
-            String localIpAddr = SystemObserver.getLocalIPAddress();
-            if ((!TextUtils.isEmpty(localIpAddr))) {
-                deviceDataObj.put(Defines.Jsonkey.LocalIP.getKey(), localIpAddr);
-            }
+        String localIpAddr = SystemObserver.getLocalIPAddress();
+        if ((!TextUtils.isEmpty(localIpAddr))) {
+            deviceDataObj.put(Defines.Jsonkey.LocalIP.getKey(), localIpAddr);
+        }
 
-            String brandName = SystemObserver.getPhoneBrand();
-            if (!isNullOrEmptyOrBlank(brandName)) {
-                deviceDataObj.put(Defines.Jsonkey.Brand.getKey(), brandName);
-            }
+        String brandName = SystemObserver.getPhoneBrand();
+        if (!isNullOrEmptyOrBlank(brandName)) {
+            deviceDataObj.put(Defines.Jsonkey.Brand.getKey(), brandName);
+        }
 
-            String appVersion = SystemObserver.getAppVersion(context_);
-            deviceDataObj.put(Defines.Jsonkey.AppVersion.getKey(), appVersion);
+        String appVersion = SystemObserver.getAppVersion(context_);
+        deviceDataObj.put(Defines.Jsonkey.AppVersion.getKey(), appVersion);
 
-            String modelName = SystemObserver.getPhoneModel();
-            if (!isNullOrEmptyOrBlank(modelName)) {
-                deviceDataObj.put(Defines.Jsonkey.Model.getKey(), modelName);
-            }
+        String modelName = SystemObserver.getPhoneModel();
+        if (!isNullOrEmptyOrBlank(modelName)) {
+            deviceDataObj.put(Defines.Jsonkey.Model.getKey(), modelName);
+        }
 
-            DisplayMetrics displayMetrics = SystemObserver.getScreenDisplay(context_);
-            deviceDataObj.put(Defines.Jsonkey.ScreenDpi.getKey(), displayMetrics.densityDpi);
-            deviceDataObj.put(Defines.Jsonkey.ScreenHeight.getKey(), displayMetrics.heightPixels);
-            deviceDataObj.put(Defines.Jsonkey.ScreenWidth.getKey(), displayMetrics.widthPixels);
-
-        } catch (JSONException ignore) { }
+        DisplayMetrics displayMetrics = SystemObserver.getScreenDisplay(context_);
+        deviceDataObj.put(Defines.Jsonkey.ScreenDpi.getKey(), displayMetrics.densityDpi);
+        deviceDataObj.put(Defines.Jsonkey.ScreenHeight.getKey(), displayMetrics.heightPixels);
+        deviceDataObj.put(Defines.Jsonkey.ScreenWidth.getKey(), displayMetrics.widthPixels);
 
         return deviceDataObj;
     }
