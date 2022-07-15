fc4731a926539687376680331529ff8b37298682
Rex Kim <rkim@branch.io>
2022-07-15 16:11:29 -0700

    [other] Migrate to build.gradle.kts (#998)
    This commit introduces the following changes:
    
    1. Migrate all groovy DSL to Kotlin DSL
    2. Add Kotlin support
    3. Use gradle's official publish and signing plugins
    4. Update Gradle Android Plugin to the latest
    
    As a result of above changes, following changes are required for a development
    
    1. JDK 11 or higher is required.

---
+++ build.gradle.kts
@@ -0,0 +1,50 @@
+import  org.gradle.api.tasks.testing.logging.*
+
+plugins {
+    id("com.android.library") version "7.2.1" apply false
+    id("com.android.application") version "7.2.1" apply false
+    kotlin("android") version "1.6.20" apply false
+}
+
+val VERSION_NAME: String by project
+val GROUP: String by project
+
+allprojects {
+    version = VERSION_NAME
+    group = GROUP
+
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        maven {
+            url = uri("https://maven.google.com") // Google"s Maven repository
+        }
+        google()
+        maven {
+            url = uri("https://developer.huawei.com/repo/") // Huawei's Maven Repository, use for compilation only
+        }
+    }
+}
+
+subprojects {
+    tasks {
+        withType<Javadoc> {
+            enabled = true
+        }
+    }
+}
+
+tasks {
+    withType<Test> {
+        testLogging {
+            exceptionFormat = TestExceptionFormat.FULL
+            events = setOf(
+                TestLogEvent.STARTED,
+                TestLogEvent.SKIPPED,
+                TestLogEvent.PASSED,
+                TestLogEvent.FAILED,
+            )
+            showStandardStreams = true
+        }
+    }
+}
