dc504f5a0c84545c4b7bfa89cfce9132caa2d53a
nsingh-branch <nipun.singh@branch.io>
2023-01-20 10:41:28 -0800

    Updated testbed look

--- Branch-SDK-TestBed/src/main/java/io/branch/branchandroiddemo/MainActivity.java
+++ Branch-SDK-TestBed/src/main/java/io/branch/branchandroiddemo/MainActivity.java
@@ -61,7 +61,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_activity);
 
         txtShortUrl = findViewById(R.id.editReferralShortUrl);
         txtInstallCount = findViewById(R.id.txtInstallCount);
