2cef512f72937a6645319c78a2d8c5c56662b24e
gdeluna-branch <94577415+gdeluna-branch@users.noreply.github.com>
2023-06-22 16:57:28 -0700

    bump test deps

--- Branch-SDK-TestBed/build.gradle.kts
+++ Branch-SDK-TestBed/build.gradle.kts
@@ -15,10 +15,10 @@ dependencies {
     }
     implementation("com.android.billingclient:billing:5.1.0")
 
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test:runner:1.4.0")
-    androidTestImplementation("androidx.test:rules:1.4.0")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test:runner:1.5.2")
+    androidTestImplementation("androidx.test:rules:1.5.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
 
 android {
