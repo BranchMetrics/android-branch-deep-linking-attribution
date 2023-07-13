8de369013a8032ec840703aecdaa01d88a449407
gdeluna-branch <94577415+gdeluna-branch@users.noreply.github.com>
2023-07-13 01:59:33 -0700

    Update DataCoroutines.kt

--- Branch-SDK/src/main/java/io/branch/referral/coroutines/DataCoroutines.kt
+++ Branch-SDK/src/main/java/io/branch/referral/coroutines/DataCoroutines.kt
@@ -1,19 +1,17 @@
 package io.branch.referral.coroutines
 
 import android.content.Context
-import android.util.Log
 import com.google.android.gms.ads.identifier.AdvertisingIdClient
+import io.branch.referral.PrefHelper
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 suspend fun getAdvertisingInfoObject(context: Context): AdvertisingIdClient.Info? {
     return withContext(Dispatchers.Default) {
         try {
-            val info = AdvertisingIdClient.getAdvertisingIdInfo(context)
-            Log.d("BranchSDK", "getAdvertisingIdInfo received: $info")
-            info
+            AdvertisingIdClient.getAdvertisingIdInfo(context)
         } catch (exception: Exception) {
-            Log.d("BranchSDK", "getAdvertisingIdInfo exception: $exception")
+            PrefHelper.Debug("getAdvertisingIdInfo exception: $exception")
             null
         }
     }
