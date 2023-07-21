52c82fef194743d59ac654524545e4df71866d3b
gdeluna-branch <gabriel.deluna@branch.io>
2023-07-21 16:14:55 -0700

    fix:Convert GAID AsyncTask to coroutine (#1084)

---
+++ Branch-SDK/src/main/java/io/branch/referral/util/DependencyUtils.kt
@@ -0,0 +1,26 @@
+package io.branch.referral.util
+
+import io.branch.referral.PrefHelper
+
+fun classExists(className: String): Boolean {
+    return try {
+        Class.forName(className)
+        true
+    } catch (e: ClassNotFoundException) {
+        PrefHelper.Debug("Could not find $className. If expected, import the dependency into your app.")
+        false
+    }
+}
+
+const val playStoreInstallReferrerClass = "com.android.installreferrer.api.InstallReferrerClient"
+
+const val playStoreAdvertisingIdClientClass =
+    "com.google.android.gms.ads.identifier.AdvertisingIdClient"
+
+const val huaweiInstallReferrerClass =
+    "com.huawei.hms.ads.installreferrer.api.InstallReferrerClient"
+
+const val galaxyStoreInstallReferrerClass =
+    "com.sec.android.app.samsungapps.installreferrer.api.InstallReferrerClient"
+
+const val xiaomiInstallReferrerClass = "com.miui.referrer.api.GetAppsReferrerClient"
