8fada2f6418aee66afdd5ddadd0beb63a5668b3c
gdeluna-branch <94577415+gdeluna-branch@users.noreply.github.com>
2023-09-27 20:59:46 -0700

    refactor for testability

--- Branch-SDK/src/main/java/io/branch/referral/network/BranchRemoteInterfaceUrlConnection.java
+++ Branch-SDK/src/main/java/io/branch/referral/network/BranchRemoteInterfaceUrlConnection.java
@@ -7,7 +7,6 @@ import android.os.NetworkOnMainThreadException;
 import android.util.Base64;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 
 import com.google.android.gms.common.util.Strings;
 
@@ -27,7 +26,6 @@ import java.io.OutputStreamWriter;
 import java.net.SocketException;
 import java.net.SocketTimeoutException;
 import java.net.URL;
-import java.util.Objects;
 
 import javax.net.ssl.HttpsURLConnection;
 import org.json.JSONException;
@@ -57,11 +55,12 @@ public class BranchRemoteInterfaceUrlConnection extends BranchRemoteInterface {
         return doRestfulPost(url, payload, 0);
     }
 
-
     ///-------------- private methods to implement RESTful GET / POST using HttpURLConnection ---------------//
     private BranchResponse doRestfulGet(String url, int retryNumber) throws BranchRemoteException {
         HttpsURLConnection connection = null;
         PrefHelper prefHelper = PrefHelper.getInstance(branch.getApplicationContext());
+        int retryInterval = prefHelper.getRetryInterval();
+
         try {
             int timeout = prefHelper.getTimeout();
             int connectTimeout = prefHelper.getConnectTimeout();
@@ -76,11 +75,7 @@ public class BranchRemoteInterfaceUrlConnection extends BranchRemoteInterface {
 
             int responseCode = connection.getResponseCode();
             if (responseCode >= 500 && retryNumber < prefHelper.getRetryCount()) {
-                try {
-                    Thread.sleep(prefHelper.getRetryInterval());
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                sleepForInterval(retryInterval);
                 retryNumber++;
                 return doRestfulGet(url, retryNumber);
             } else {
@@ -106,11 +101,7 @@ public class BranchRemoteInterfaceUrlConnection extends BranchRemoteInterface {
         } catch (SocketTimeoutException ex) {
             // On socket  time out retry the request for retryNumber of times
             if (retryNumber < prefHelper.getRetryCount()) {
-                try {
-                    Thread.sleep(prefHelper.getRetryInterval());
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                sleepForInterval(retryInterval);
                 retryNumber++;
                 return doRestfulGet(url, retryNumber);
             } else {
@@ -119,11 +110,7 @@ public class BranchRemoteInterfaceUrlConnection extends BranchRemoteInterface {
         } catch(InterruptedIOException ex){
             // When the thread times out before or while sending the request
             if (retryNumber < prefHelper.getRetryCount()) {
-                try {
-                    Thread.sleep(prefHelper.getRetryInterval());
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                sleepForInterval(retryInterval);
                 retryNumber++;
                 return doRestfulGet(url, retryNumber);
             } else {
@@ -143,138 +130,148 @@ public class BranchRemoteInterfaceUrlConnection extends BranchRemoteInterface {
     private BranchResponse doRestfulPost(String url, JSONObject payload, int retryNumber) throws BranchRemoteException {
         HttpsURLConnection connection = null;
         PrefHelper prefHelper = PrefHelper.getInstance(branch.getApplicationContext());
+
         int timeout = prefHelper.getTimeout();
         int connectTimeout = prefHelper.getConnectTimeout();
+        int retryInterval = branch.getPrefHelper().getRetryInterval();
+        int retryLimit = prefHelper.getRetryCount();
 
         try {
             payload.put(RETRY_NUMBER, retryNumber);
-        } catch (JSONException e) {
+        }
+        catch (JSONException e) {
             BranchLogger.d(e.getMessage());
         }
-        try {
-            // set the setThreadStatsTag for POST if API 26+
-            if (android.os.Build.VERSION.SDK_INT >= 26) {
-                TrafficStats.setThreadStatsTag(THREAD_TAG_POST);
-            }
 
-            URL urlObject = new URL(url);
-            connection = (HttpsURLConnection) urlObject.openConnection();
-            connection.setConnectTimeout(connectTimeout);
-            connection.setReadTimeout(timeout);
-            connection.setDoInput(true);
-            connection.setDoOutput(true);
-            if (url.contains(Defines.Jsonkey.QRCodeTag.getKey())) {
-                connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-                connection.setRequestProperty("Accept", "image/*");
-            } else {
-                connection.setRequestProperty("Content-Type", "application/json");
-                connection.setRequestProperty("Accept", "application/json");
-             }
-            connection.setRequestMethod("POST");
+        BranchResponse result = null;
 
-            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(connection.getOutputStream());
-
-            outputStreamWriter.write(payload.toString());
-            outputStreamWriter.flush();
-            outputStreamWriter.close();
-
-            String requestId = connection.getHeaderField(Defines.HeaderKey.RequestId.getKey());
-
-            int responseCode = connection.getResponseCode();
-            if (responseCode >= HttpsURLConnection.HTTP_INTERNAL_ERROR
-                    && retryNumber < prefHelper.getRetryCount()) {
-                try {
-                    Thread.sleep(prefHelper.getRetryInterval());
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+        do {
+            try {
+                // set the setThreadStatsTag for POST if API 26+
+                if (android.os.Build.VERSION.SDK_INT >= 26) {
+                    TrafficStats.setThreadStatsTag(THREAD_TAG_POST);
                 }
-                retryNumber++;
-                return doRestfulPost(url, payload, retryNumber);
-            } else {
-                BranchResponse result;
-                try {
-                    if (responseCode != HttpsURLConnection.HTTP_OK && connection.getErrorStream() != null) {
-                        result = new BranchResponse(getResponseString(connection.getErrorStream()), responseCode);
-                    } else {
-                        if (url.contains(Defines.Jsonkey.QRCodeTag.getKey())) {
-                            // Converting binary data to Base64
-                            InputStream inputStream = connection.getInputStream();
-                            Bitmap bmp = BitmapFactory.decodeStream(inputStream);
-                            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                            bmp.compress(Bitmap.CompressFormat.PNG, 100, baos);
-                            byte[] b = baos.toByteArray();
-                            String bmpString = Base64.encodeToString(b, Base64.DEFAULT);
 
-                            result = new BranchResponse(bmpString, responseCode);
-                        } else {
-                            result = new BranchResponse(getResponseString(connection.getInputStream()), responseCode);
+                URL urlObject = new URL(url);
+                connection = (HttpsURLConnection) urlObject.openConnection();
+                connection.setConnectTimeout(connectTimeout);
+                connection.setReadTimeout(timeout);
+                connection.setDoInput(true);
+                connection.setDoOutput(true);
+                if (url.contains(Defines.Jsonkey.QRCodeTag.getKey())) {
+                    connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                    connection.setRequestProperty("Accept", "image/*");
+                }
+                else {
+                    connection.setRequestProperty("Content-Type", "application/json");
+                    connection.setRequestProperty("Accept", "application/json");
+                }
+                connection.setRequestMethod("POST");
+
+                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(connection.getOutputStream());
+
+                outputStreamWriter.write(payload.toString());
+                outputStreamWriter.flush();
+                outputStreamWriter.close();
+
+                String requestId = connection.getHeaderField(Defines.HeaderKey.RequestId.getKey());
+
+                int responseCode = connection.getResponseCode();
+
+                // If we encounter a 5XX error from the service, retry
+                if (responseCode >= HttpsURLConnection.HTTP_INTERNAL_ERROR && shouldRetry(retryNumber, retryLimit)) {
+                    sleepForInterval(retryInterval);
+                    retryNumber++;
+                }
+                // If it was not a 5XX, or we cannot retry
+                else {
+                    try {
+                        // If it was not a 200, and the error is not null
+                        // This captures the un-retryable 4XX response
+                        if (responseCode != HttpsURLConnection.HTTP_OK && connection.getErrorStream() != null) {
+                            result = new BranchResponse(getResponseString(connection.getErrorStream()), responseCode);
+                        }
+                        else {
+                            // If was a 200, or the error was null
+                            // Check if this is a response to QR code request and parse
+                            if (url.contains(Defines.Jsonkey.QRCodeTag.getKey())) {
+                                // Converting binary data to Base64
+                                InputStream inputStream = connection.getInputStream();
+                                Bitmap bmp = BitmapFactory.decodeStream(inputStream);
+                                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                                bmp.compress(Bitmap.CompressFormat.PNG, 100, baos);
+                                byte[] b = baos.toByteArray();
+                                String bmpString = Base64.encodeToString(b, Base64.DEFAULT);
+
+                                result = new BranchResponse(bmpString, responseCode);
+                            }
+                            // Read out the response
+                            else {
+                                result = new BranchResponse(getResponseString(connection.getInputStream()), responseCode);
+                            }
                         }
                     }
+                    catch (FileNotFoundException ex) {
+                        // In case of Resource conflict getInputStream will throw FileNotFoundException. Handle it here in order to send the right status code
+                        BranchLogger.v("A resource conflict occurred with this request " + url);
+                        result = new BranchResponse(null, responseCode);
+                    }
 
-                } catch (FileNotFoundException ex) {
-                    // In case of Resource conflict getInputStream will throw FileNotFoundException. Handle it here in order to send the right status code
-                    BranchLogger.v("A resource conflict occurred with this request " + url);
-                    result = new BranchResponse(null, responseCode);
+                    result.requestId = requestId;
+                    break;
                 }
-
-                result.requestId = requestId;
-                return result;
             }
 
-
-        } catch (SocketTimeoutException ex) {
-            // On socket  time out retry the request for retryNumber of times
-            if (retryNumber < prefHelper.getRetryCount()) {
-                try {
-                    Thread.sleep(prefHelper.getRetryInterval());
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+            catch (Exception ex) {
+                if(!shouldRetry(retryNumber, retryLimit)) {
+                    BranchLogger.v("Exception: " + ex.getMessage());
+                    handleException(ex);
+                    result = new BranchResponse(null, 500);
                 }
+            }
+            finally {
+                sleepForInterval(retryInterval);
                 retryNumber++;
-                return doRestfulPost(url, payload, retryNumber);
-            } else {
-                throw new BranchRemoteException(BranchError.ERR_BRANCH_REQ_TIMED_OUT);
+
+                if (connection != null) {
+                    connection.disconnect();
+                }
             }
-        } catch(InterruptedIOException ex){
+
+            BranchLogger.v("BranchRemoteInterfaceUrlConnection end do retryCount=" + retryNumber + " result= " + result);
+        } while (shouldRetry(retryNumber, retryLimit) || result != null);
+
+        return result;
+    }
+
+    private void sleepForInterval(int interval) {
+        try {
+            Thread.sleep(interval);
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean shouldRetry(int retryCount, int retryLimit){
+        return retryCount < retryLimit;
+    }
+
+    private void handleException(Exception ex) throws BranchRemoteException {
+        if (ex instanceof SocketTimeoutException) {
+            // On socket time out retry the request for retryNumber of times
+            throw new BranchRemoteException(BranchError.ERR_BRANCH_REQ_TIMED_OUT);
+        }
+        else if (ex instanceof InterruptedIOException) {
             // When the thread times out before or while sending the request
-            if (retryNumber < prefHelper.getRetryCount()) {
-                try {
-                    Thread.sleep(prefHelper.getRetryInterval());
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                retryNumber++;
-                return doRestfulPost(url, payload, retryNumber);
-            } else {
-                throw new BranchRemoteException(BranchError.ERR_BRANCH_TASK_TIMEOUT);
-            }
+            throw new BranchRemoteException(BranchError.ERR_BRANCH_TASK_TIMEOUT);
         }
         // Unable to resolve host/Unknown host exception
-        catch (IOException ex) {
-            if (retryNumber < prefHelper.getRetryCount()) {
-                try {
-                    Thread.sleep(prefHelper.getRetryInterval());
-                }
-                catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                retryNumber++;
-                return doRestfulPost(url, payload, retryNumber);
-            }
-            else {
-                throw new BranchRemoteException(BranchError.ERR_BRANCH_NO_CONNECTIVITY);
-            }
-        } catch (Exception ex) {
-            BranchLogger.v("Exception: " + ex.getMessage());
-            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.HONEYCOMB) {
-                if (ex instanceof NetworkOnMainThreadException)
-                    BranchLogger.v("Branch Error: Don't call our synchronous methods on the main thread!!!");
-            }
-            return new BranchResponse(null, 500);
-        } finally {
-            if (connection != null) {
-                connection.disconnect();
-            }
+        else if (ex instanceof IOException) {
+            throw new BranchRemoteException(BranchError.ERR_BRANCH_NO_CONNECTIVITY);
+        }
+        if (ex instanceof NetworkOnMainThreadException) {
+            BranchLogger.e("Branch Error: Cannot call network on main thread.");
         }
     }
 
