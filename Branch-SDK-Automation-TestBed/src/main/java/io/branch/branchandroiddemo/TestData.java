c16c3297fffff4ce9937e4186fec84d779c328b5
NidhiDixit09 <93544270+NidhiDixit09@users.noreply.github.com>
2024-04-24 10:09:01 -0700

    Removed Extra Code(Extra Screens - BUO, short URL metadata etc), changed package name, added buttons of initializing session, Removed Navigation and Read Links buttons

---
+++ Branch-SDK-Automation-TestBed/src/main/java/io/branch/branchandroiddemo/TestData.java
@@ -0,0 +1,182 @@
+package io.branch.branchandroiddemo;
+
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.google.gson.Gson;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.util.Calendar;
+
+import io.branch.indexing.BranchUniversalObject;
+import io.branch.referral.util.BRANCH_STANDARD_EVENT;
+import io.branch.referral.util.BranchContentSchema;
+import io.branch.referral.util.BranchEvent;
+import io.branch.referral.util.ContentMetadata;
+import io.branch.referral.util.CurrencyType;
+import io.branch.referral.util.LinkProperties;
+import io.branch.referral.util.ProductCategory;
+
+public class TestData {
+
+    public BranchUniversalObject getParamBUOObject(String testData){
+        BranchUniversalObject buoObject = null;
+        try {
+            JSONObject testDataObj = null;
+
+            testDataObj = (JSONObject) new JSONParser().parse(testData);
+
+            if (testDataObj.containsKey("BUOData")){
+                JSONObject j = (JSONObject) testDataObj.get("BUOData");
+
+                ContentMetadata contentMetadata = null;
+                Gson gson = new Gson();
+                BUOData buoData = gson.fromJson(j.toString(), BUOData.class);
+
+                if (buoData != null) {
+                    contentMetadata = new ContentMetadata()
+                            .setProductName(buoData.productName)
+                            .setProductBrand(buoData.productBrand)
+                            .setProductVariant(buoData.productVariant)
+                            .setProductCondition(TextUtils.isEmpty(buoData.productCondition) ? null : ContentMetadata.CONDITION.valueOf(buoData.productCondition))
+                            .setProductCategory(TextUtils.isEmpty(buoData.productCategory) ? null : ProductCategory.valueOf(buoData.productCategory))
+                            .setAddress(buoData.street, buoData.city, buoData.region, buoData.country, buoData.postalCode)
+                            .setLocation(TextUtils.isEmpty(buoData.latitude) ? null : Double.parseDouble(buoData.latitude), TextUtils.isEmpty(buoData.longitude) ? null : Double.parseDouble(buoData.longitude))
+                            .setSku(buoData.sku)
+                            .setRating(TextUtils.isEmpty(buoData.rating) ? null : Double.parseDouble(buoData.rating),
+                                    TextUtils.isEmpty(buoData.averageRating) ? null : Double.parseDouble(buoData.averageRating),
+                                    TextUtils.isEmpty(buoData.maximumRating) ? null : Double.parseDouble(buoData.maximumRating),
+                                    TextUtils.isEmpty(buoData.ratingCount) ? null : Integer.parseInt(buoData.ratingCount))
+                            .addImageCaptions(buoData.imageCaption)
+                            .setQuantity(TextUtils.isEmpty(buoData.quantity) ? null : Double.parseDouble(buoData.quantity))
+                            .setPrice(TextUtils.isEmpty(buoData.price) ? null : Double.parseDouble(buoData.price), TextUtils.isEmpty(buoData.currencyType) ? CurrencyType.USD : CurrencyType.valueOf(buoData.currencyType))
+                            .setContentSchema(TextUtils.isEmpty(buoData.contentSchema) ? null : BranchContentSchema.valueOf(buoData.contentSchema))
+                            .addCustomMetadata("Custom_Content_metadata_key1", buoData.customMetadata);
+
+                    buoObject = new BranchUniversalObject()
+                            .setCanonicalIdentifier(buoData.canonicalIdentifier)
+                            .setTitle(buoData.contentTitle)
+                            .setContentDescription(buoData.contentDesc)
+                            .setContentImageUrl(buoData.imageUrl)
+                            .setContentIndexingMode(BranchUniversalObject.CONTENT_INDEX_MODE.PUBLIC)
+                            .setLocalIndexMode(BranchUniversalObject.CONTENT_INDEX_MODE.PUBLIC)
+                            .setContentMetadata(contentMetadata);
+                }
+            }
+            Log.i("BRANCH SDK error", "No BUO");
+        } catch (ParseException e) {
+            System.out.println("position: " + e.getPosition());
+            System.out.println(e);
+        }
+        return buoObject;
+    }
+    public LinkProperties getParamLinkPropertiesObject(String testData){
+        LinkProperties linkProperties = null;
+        try {
+            JSONObject testDataObj = null;
+            testDataObj = (JSONObject) new JSONParser().parse(testData);
+            if (testDataObj.containsKey("CreateLinkReference")) {
+                JSONObject lpJSONObj = (JSONObject) testDataObj.get("CreateLinkReference");
+                Gson gson = new Gson();
+                LinkPropertiesData lpData = gson.fromJson(lpJSONObj.toString(), LinkPropertiesData.class);
+                if (lpData != null) {
+                    linkProperties = new LinkProperties()
+                            .setChannel(TextUtils.isEmpty(lpData.channelName) ? null : lpData.channelName)
+                            .setFeature(TextUtils.isEmpty(lpData.feature) ? null : lpData.feature)
+                            .setCampaign(TextUtils.isEmpty(lpData.campaign) ? null : lpData.campaign)
+                            .setStage(TextUtils.isEmpty(lpData.stage) ? null : lpData.stage)
+                            .addControlParameter("$desktop_url", TextUtils.isEmpty(lpData.desktopUrl) ? null : lpData.desktopUrl)
+                            .addControlParameter("$android_url", TextUtils.isEmpty(lpData.androidUrl) ? null : lpData.androidUrl)
+                            .addControlParameter("$fallback_url", TextUtils.isEmpty(lpData.androidUrl) ? null : lpData.androidUrl)
+                            .addControlParameter("custom", TextUtils.isEmpty(lpData.additionalData) ? null : lpData.additionalData)
+                            .addControlParameter("$ios_url", TextUtils.isEmpty(lpData.iOSUrl) ? null : lpData.iOSUrl)
+                            .addControlParameter("custom_random", Long.toString(Calendar.getInstance().getTimeInMillis()));
+                }
+            }
+        } catch (ParseException pe) {
+            System.out.println("position: " + pe.getPosition());
+            System.out.println(pe);
+        } catch (Exception e) {
+            Log.i("BRANCH SDK error", e.toString());
+        }
+
+        return linkProperties;
+    }
+
+    public BranchEvent getBranchEventObject(String testData){
+        BranchEvent branchEvent = null;
+        try {
+            JSONObject testDataObj = null;
+            testDataObj = (JSONObject) new JSONParser().parse(testData);
+            if (testDataObj.containsKey("TrackContentData")) {
+                JSONObject evJSONObj = (JSONObject) testDataObj.get("TrackContentData");
+                Gson gson = new Gson();
+                BranchEventData eventData = gson.fromJson(evJSONObj.toString(), BranchEventData.class);
+                if (eventData != null) {
+                    branchEvent = new BranchEvent(BRANCH_STANDARD_EVENT.valueOf(TextUtils.isEmpty(eventData.event) ? BRANCH_STANDARD_EVENT.ADD_TO_CART.toString() : eventData.event));
+                }
+            }
+        } catch (ParseException pe) {
+            System.out.println("position: " + pe.getPosition());
+            System.out.println(pe);
+        } catch (Exception e) {
+            Log.i("BRANCH SDK error", e.toString());
+        }
+
+        return branchEvent;
+    }
+
+    public String getUserName(String testData){
+        try {
+            JSONObject testDataObj = null;
+            testDataObj = (JSONObject) new JSONParser().parse(testData);
+            if (testDataObj.containsKey("UserName")) {
+                String username = (String) testDataObj.get("UserName");
+                return username;
+            }
+        } catch (ParseException pe) {
+            System.out.println("position: " + pe.getPosition());
+            System.out.println(pe);
+        } catch (Exception e) {
+            Log.i("BRANCH SDK error", e.toString());
+        }
+        return null;
+    }
+
+    public String getParamValue(String testData, String key){
+        try {
+            JSONObject testDataObj = null;
+            testDataObj = (JSONObject) new JSONParser().parse(testData);
+            if (testDataObj.containsKey(key)) {
+                String value = (String) testDataObj.get(key);
+                return value;
+            }
+        } catch (ParseException pe) {
+            System.out.println("position: " + pe.getPosition());
+            System.out.println(pe);
+        } catch (Exception e) {
+            Log.i("BRANCH SDK error", e.toString());
+        }
+        return null;
+    }
+
+    public Boolean getBoolParamValue(String testData, String key){
+        try {
+            JSONObject testDataObj = null;
+            testDataObj = (JSONObject) new JSONParser().parse(testData);
+            if (testDataObj.containsKey(key)) {
+                Boolean boolValue = (Boolean) testDataObj.get(key);
+                return boolValue;
+            }
+        } catch (ParseException pe) {
+            System.out.println("position: " + pe.getPosition());
+            System.out.println(pe);
+        } catch (Exception e) {
+            Log.i("BRANCH SDK error", e.toString());
+        }
+        return false;
+    }
+}
---
+++ Branch-SDK-Automation-TestBed/src/main/java/io/branch/branchandroiddemo/activities/MainActivity.java
@@ -0,0 +1,118 @@
+package io.branch.branchandroiddemo.activities;
+
+import  io.branch.branchandroiddemo.BranchWrapper;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import io.branch.branchandroiddemo.BranchWrapper;
+import io.branch.indexing.BranchUniversalObject;
+import io.branch.referral.Branch;
+import io.branch.referral.BranchError;
+import io.branch.referral.util.LinkProperties;
+import io.branch.branchandroiddemo.Common;
+import io.branch.branchandroiddemo.Constants;
+import io.branch.branchandroiddemo.R;
+import io.branch.branchandroiddemo.TestData;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button  btCreateDeepLink, btNativeShare, btTrackUser,
+             btCreateQrCode, btSetDMAParams, btLogEvent, btReadLogs, btInitSession;
+    private TextView tvMessage, tvUrl;
+    ToggleButton trackingCntrlBtn;
+
+    BranchWrapper branchWrapper;
+    protected void initialize(){
+        btCreateDeepLink = findViewById(R.id.bt_create_deep_link);
+        btNativeShare = findViewById(R.id.bt_native_share);
+        tvMessage = findViewById(R.id.tv_message);
+        tvUrl = findViewById(R.id.tv_url);
+        btCreateQrCode = findViewById(R.id.bt_create_qr_code);
+        btSetDMAParams = findViewById(R.id.bt_set_dma_params);
+        btLogEvent = findViewById(R.id.bt_logEvent_with_callback);
+        btReadLogs = findViewById(R.id.bt_Read_Logs);
+        btTrackUser = findViewById(R.id.bt_track_user);
+        btInitSession = findViewById(R.id.bt_init_session);
+        trackingCntrlBtn = findViewById(R.id.tracking_cntrl_btn);
+
+        btCreateDeepLink.setOnClickListener(this);
+        btNativeShare.setOnClickListener(this);
+        btCreateQrCode.setOnClickListener(this);
+        btSetDMAParams.setOnClickListener(this);
+        btLogEvent.setOnClickListener(this);
+        btReadLogs.setOnClickListener(this);
+        btTrackUser.setOnClickListener(this);
+        btInitSession.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Common.getInstance().clearLog();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initialize();
+
+        branchWrapper = new BranchWrapper(getApplicationContext());
+        branchWrapper.delayInitializationIfRequired(getIntent());
+
+        Branch.enableLogging();
+
+        trackingCntrlBtn.setChecked(Branch.getInstance().isTrackingDisabled());
+        trackingCntrlBtn.setOnCheckedChangeListener((buttonView, isChecked) -> Branch.getInstance().disableTracking(isChecked));
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        getIntent().putExtra("testData", "{\"BUOData\":{\"imageUrl\":\"https:\\/\\/www.qentelli.com\\/sites\\/default\\/files\\/qentelli-logo.svg\",\"canonicalIdentifier\":\"content\\/2\",\"contentTitle\":\"Sample Content Title\",\"contentDesc\":\"Sample Content Description\"},\"Include\":\"Yes\",\"CreateLinkReference\":{\"feature\":\"sharing\",\"stage\":\"new user\",\"desktopUrl\":\"www.qentelli.com\",\"androidUrl\":\"https:\\/\\/play.google.com\\/store\\/apps\\/details?id=com.instagram.android\",\"campaign\":\"content 2 launch\",\"channelName\":\"instagram\",\"additionalData\":\"When all fields provided with valid entries\",\"iOSUrl\":\"www.qentelli.com\"},\"TrackContentData\" : {\n" +
+                "         \"eventType\": \"COMMERCE\",\n" +
+                "         \"trackContentEvent\" : \"ADD_TO_CART\",\n" +
+                "         \"alias\" : \"My Alias\",\n" +
+                "         \"transactionId\": \"Branch007XYZ\",\n" +
+                "         \"currency\": \"AED\",\n" +
+                "         \"revenue\": \"10000\",\n" +
+                "         \"shipping\": \"12345\",\n" +
+                "         \"tax\": \"100\",\n" +
+                "         \"coupon\": \"BranchCoupon\",\n" +
+                "         \"affiliation\": \"Affiliation\",\n" +
+                "         \"description\": \"Branch Track Content Desc\",\n" +
+                "         \"purchaseLocation\": \"US\",\n" +
+                "         \"availability\": \"available\",\n" +
+                "         \"additionalData\": \"Track content Commerce Additional data\",\n" +
+                "         \"searchQuery\": \"nothing\",\n" +
+                "         \"registrationId\": \"AEU10EEEE1123\",\n" +
+                "         \"customEventProperty1\": \"My custom event1\",\n" +
+                "         \"customEventProperty2\": \"My custom event2\",\n" +
+                "         \"customData1\": \"Branch custom data1\",\n" +
+                "         \"customData2\": \"Branch custom data2\"\n" +
+                "       }, \"UserName\" : \"test_automation_user1\", \"URL\": \"https://cq9pf.app.link/1JW565m0XIb\" }");
+        https://cq9pf.app.link/lP3DUMFb3Ib
+        if (view == btCreateDeepLink) {
+            branchWrapper.createDeepLink(getIntent(), MainActivity.this);
+        } else if (view == btNativeShare) {
+            branchWrapper.nativeShare(this, getIntent(), MainActivity.this);
+        } else if (view == btLogEvent) {
+            branchWrapper.logEvent(getIntent(), MainActivity.this);
+        } else if (view == btTrackUser) {
+            branchWrapper.setIdentity(getIntent(), MainActivity.this);
+        } else if (view == btSetDMAParams) {
+            branchWrapper.setDMAParams(getIntent());
+        } else if (view == btInitSession) {
+            branchWrapper.initSession(this, getIntent(), MainActivity.this);
+        } else if (view == btReadLogs) {
+            branchWrapper.showLogWindow("",false, this, Constants.LOG_DATA);
+        } else if (view == btCreateQrCode) {
+            branchWrapper.getQRCode(this, getIntent(), MainActivity.this);
+        }else {
+            branchWrapper.showLogWindow("",false, this, Constants.UNKNOWN);
+        }
+    }
+}
