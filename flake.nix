6b248deb0fd03a6a5c11456b84374f337f78cb05
Allen <allen.eubank@branch.io>
2024-08-02 09:06:44 -0500

    consolidate into flake.nix

--- flake.nix
+++ flake.nix
@@ -3,63 +3,54 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
-    devshell.url = "github:numtide/devshell";
     flake-utils.url = "github:numtide/flake-utils";
     android.url = "github:tadfisher/android-nixpkgs";
   };
 
-  outputs = { self, nixpkgs, devshell, flake-utils, android }:
-    {
-      overlay = final: prev: {
-        inherit (self.packages.${final.system}) android-sdk;
+  outputs = { self, nixpkgs, flake-utils, android }: flake-utils.lib.eachSystem [ "aarch64-darwin" "x86_64-darwin" "x86_64-linux" ] (system:
+    let
+      inherit (nixpkgs) lib;
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+        overlays = [
+          (final: prev: {
+            android-sdk = android.sdk.${system} (sdkPkgs: with sdkPkgs; [
+              build-tools-34-0-0
+              cmdline-tools-latest
+              emulator
+              platform-tools
+              platforms-android-34
+            ]
+            ++ lib.optionals (system == "aarch64-darwin") [
+              system-images-android-34-google-apis-arm64-v8a
+            ]
+            ++ lib.optionals (system == "x86_64-darwin" || system == "x86_64-linux") [
+              system-images-android-34-google-apis-x86-64
+            ]);
+          })
+        ];
       };
-    }
-    //
-    flake-utils.lib.eachSystem [ "aarch64-darwin" "x86_64-darwin" "x86_64-linux" ] (system:
-      let
-        inherit (nixpkgs) lib;
-        pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-          overlays = [
-            devshell.overlays.default
-            self.overlay
+    in
+    {
+      formatter = pkgs.nixpkgs-fmt;
+      devShells = {
+        default = pkgs.mkShell {
+          name = "branch-sdk-android";
+          ANDROID_HOME = "${pkgs.android-sdk}/share/android-sdk";
+          ANDROID_SDK_ROOT = "${pkgs.android-sdk}/share/android-sdk";
+          JAVA_HOME = "${pkgs.jdk17.home}";
+          shellHook = ''
+            echo "
+            Entered the Branch SDK Android development environment.
+            ";
+          '';
+          packages = [
+            pkgs.android-sdk
+            pkgs.jdk17 # for android build-tools
           ];
         };
-      in
-      {
-        packages = {
-          android-sdk = android.sdk.${system} (sdkPkgs: with sdkPkgs; [
-            # Useful packages for building and testing.
-            build-tools-34-0-0
-            # build-tools-30-0-3
-            cmdline-tools-latest
-            emulator
-            platform-tools
-            platforms-android-34
-
-            # Other useful packages for a development environment.
-            # ndk-26-1-10909125
-            # skiaparser-3
-            # sources-android-34
-          ]
-          ++ lib.optionals (system == "aarch64-darwin") [
-            system-images-android-34-google-apis-arm64-v8a
-            # system-images-android-34-google-apis-playstore-arm64-v8a
-          ]
-          ++ lib.optionals (system == "x86_64-darwin" || system == "x86_64-linux") [
-            system-images-android-34-google-apis-x86-64
-            # system-images-android-34-google-apis-playstore-x86-64
-          ]);
-        } // lib.optionalAttrs (system == "x86_64-linux") {
-          # Android Studio in nixpkgs is currently packaged for x86_64-linux only.
-          # android-studio = pkgs.androidStudioPackages.stable;
-          # android-studio = pkgs.androidStudioPackages.beta;
-          # android-studio = pkgs.androidStudioPackages.preview;
-          # android-studio = pkgs.androidStudioPackage.canary;
-        };
-
-        devShell = import ./devshell.nix { inherit pkgs; };
-      }
-    );
+      };
+    }
+  );
 }
