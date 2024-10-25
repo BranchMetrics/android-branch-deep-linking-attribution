bfb9f04063ab9cbc4dc48d611b239df1a9f486a7
rob-gioia-branch <robert.gioia@branch.io>
2024-10-25 15:09:16 -0400

    Added code to generate the various types of Branch links for testing
    Added code to generate the various types of Branch links for testing

--- Branch-SDK/src/main/java/io/branch/referral/validators/LinkingValidatorDialog.java
+++ Branch-SDK/src/main/java/io/branch/referral/validators/LinkingValidatorDialog.java
@@ -15,7 +15,9 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
+import io.branch.indexing.BranchUniversalObject;
 import io.branch.referral.R;
+import io.branch.referral.util.LinkProperties;
 
 public class LinkingValidatorDialog extends Dialog implements AdapterView.OnItemSelectedListener {
 
@@ -27,16 +29,26 @@ public class LinkingValidatorDialog extends Dialog implements AdapterView.OnItem
     private EditText linkingValidatorEditText;
     private LinearLayout customKVPField;
     private int step = 1;
+    private String routingKey = "";
+    private String routingValue = "";
+    private EditText customKeyEditText;
+    private EditText customValueEditText;
+    private Context context;
+    private String regularBranchLink; //ordinary Branch link to test App Links / Universal Links
+    private String uriFallbackBranchLink; //Branch link with URI redirect mode of 2
+    private String webOnlyBranchLink; //Web-only Branch link
+    private String missingDeeplinkDataBranchLink; //Branch link with empty deep link data (to test graceful handling when data is missing)
 
     public LinkingValidatorDialog(final Context context) {
         super(context);
+        this.context = context;
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.setContentView(R.layout.dialog_linking_validator);
 
         linkingValidatorDropdownMenu = findViewById(R.id.linkingValidatorDropdownMenu);
         List<String> choices = new ArrayList<>();
-        choices.add("$canonical_url");
-        choices.add("$deeplink_path");
+        choices.add(LinkingValidatorConstants.canonicalUrlKey);
+        choices.add(LinkingValidatorConstants.deeplinkPathKey);
         choices.add("other (custom)");
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(context, android.R.layout.simple_spinner_item, choices);
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -93,10 +105,14 @@ public class LinkingValidatorDialog extends Dialog implements AdapterView.OnItem
             case CANONICAL_URL:
                 linkingValidatorEditText.setVisibility(View.VISIBLE);
                 linkingValidatorText.setText(LinkingValidatorConstants.canonicalURLPromptText);
+                routingKey = LinkingValidatorConstants.canonicalUrlKey;
+                routingValue = linkingValidatorEditText.getText().toString();
                 break;
             case DEEPLINK_PATH:
                 linkingValidatorEditText.setVisibility(View.VISIBLE);
                 linkingValidatorText.setText(LinkingValidatorConstants.deeplinkPathPromptText);
+                routingKey = LinkingValidatorConstants.deeplinkPathKey;
+                routingValue = linkingValidatorEditText.getText().toString();
                 break;
             case CUSTOM:
                 customKVPField.setVisibility(View.VISIBLE);
@@ -106,6 +122,27 @@ public class LinkingValidatorDialog extends Dialog implements AdapterView.OnItem
     }
 
     void GenerateBranchLinks() {
+        customKeyEditText = findViewById(R.id.keyEditText);
+        customValueEditText = findViewById(R.id.valueEditText);
 
+        //if routing key is empty, it is a custom key outside of $canonical_url and $deeplink_path
+        if(routingKey.isEmpty()) {
+            routingKey = customKeyEditText.getText().toString();
+            routingValue = customValueEditText.getText().toString();
+        }
+
+        regularBranchLink = GenerateBranchLink("regularBranchLink", routingKey, routingValue);
+        uriFallbackBranchLink = GenerateBranchLink("uriFallbackBranchLink", "$uri_redirect_mode", "2", routingKey, routingValue);
+        webOnlyBranchLink = GenerateBranchLink("webOnlyBranchLink", "$web_only", "true", routingKey, routingValue);
+        missingDeeplinkDataBranchLink = GenerateBranchLink("missingDataBranchLink", routingKey, "");
+    }
+
+    String GenerateBranchLink(String identifierForBUO, String... params) {
+        LinkProperties lp = new LinkProperties();
+        for(int i = 0; i < params.length; i+=2) {
+            lp.addControlParameter(params[i], params[i+1]);
+        }
+        BranchUniversalObject buo = new BranchUniversalObject().setCanonicalIdentifier(identifierForBUO);
+        return buo.getShortUrl(context, lp);
     }
 }
