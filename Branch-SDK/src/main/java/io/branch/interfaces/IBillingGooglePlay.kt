2b55db5ffbe92930c3c0f2c67a8999ede91601f1
gdeluna-branch <gabriel.deluna@branch.io>
2025-09-30 01:00:56 -0700

    Delete IBillingGooglePlay.kt

--- Branch-SDK/src/main/java/io/branch/interfaces/IBillingGooglePlay.kt
+++
@@ -1,9 +0,0 @@
-package io.branch.interfaces
-
-import android.content.Context
-import com.android.billingclient.api.Purchase
-
-public interface IBillingGooglePlay {
-    fun logEventWithPurchase(context: Context, purchase: Purchase);
-    fun startBillingClient(callback: (Boolean) -> Unit);
-}
